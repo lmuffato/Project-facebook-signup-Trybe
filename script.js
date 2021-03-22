5dc5e88730addb7b1520b89b5ab52cc46c6eff6d
andy silva <andssilva.23@gmail.com>
2021-03-22 18:09:02 -0400

    requisito 19

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ function alertLoginButton() {
 }
 
 buttonLogin.addEventListener('click', alertLoginButton);
+
+const radioFemale = document.getElementById('feminino');
+const radioMale = document.getElementById('masculino');
+const radioCustomized = document.getElementById('personalizado');
+const divInputCustomized = document.getElementById('customized-container');
+
+function selectCustomized() {
+  const inputCustomized = document.createElement('input');
+  inputCustomized.type = 'text';
+  inputCustomized.name = 'gender-custom';
+  inputCustomized.placeholder = 'Gênero (opcional)';
+  inputCustomized.id = 'input-customized';
+  divInputCustomized.appendChild(inputCustomized);
+}
+
+function removeCustomized() {
+  const inputCustomized = document.getElementById('input-customized');
+  if (inputCustomized) {
+    divInputCustomized.removeChild(inputCustomized);
+  }
+}
+
+radioCustomized.addEventListener('change', selectCustomized);
+
+radioFemale.addEventListener('change', removeCustomized);
+
+radioMale.addEventListener('change', removeCustomized);
