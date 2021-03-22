59b70e166757c7745444a914259ca2781fd3c7e8
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-03-22 19:34:01 -0300

    exercicio 2 e 3

--- script.js
+++
@@ -1,7 +0,0 @@
-function addLogo() {
-  const takeLogo = document.querySelector('header');
-  const takeImg = document.querySelector('img');
-  takeImg.src = 'imgs/facebook-logo.png';
-  takeLogo.appendChild(takeImg);
-}
-addLogo();
