7fc46a0057722d02e8a51cf3c270454e97dec266
Stanrley Barbosa <3011stan@gmail.com>
2021-03-22 18:35:01 -0300

    Correçao lint

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let getButtonEntrar = document.querySelector('#button-login');
-
-getButtonEntrar.addEventListener('click', spamAlert);
+const getButtonEntrar = document.querySelector('#button-login');
 
 function spamAlert() {
   alert(document.querySelector('#user-email-phone').value);
 }
+
+getButtonEntrar.addEventListener('click', spamAlert);
