1195cf1595e189c60f08238698220ef5897e264d
Matheus Lima Pimenta <mateus.krauss@gmail.com>
2021-03-22 18:46:57 -0300

    Corrige lint linha 3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   const btnLogin = document.getElementById('button-login');
-    btnLogin.addEventListener('click', () => {
+  btnLogin.addEventListener('click', () => {
     const inputName = document.getElementById('user-email-phone-label').value;
     const inputPw = document.getElementById('user-password-label').value;
     alert(`Login: ${inputName}  Senha:  ${inputPw}`);
