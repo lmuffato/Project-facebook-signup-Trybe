6dd2f5420157eea8f340338ec094a55c29a8985b
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-03-22 18:55:55 -0300

    requisito 18 fixed"

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const entrarButton = document.getElementById('button-login');
 
+
 function alertButton() {
   const valorEmail = document.getElementById('user-email-phone').value;
   alert(valorEmail);
 }
 
+
+//codigo encontrado no site https://stackoverflow.com/questions/5272433/html5-form-required-attribute-set-custom-validation-message 
+// Author: robertc
+
+function errorMessage() {
+  const registerInputs = document.getElementsByTagName('input')
+  for (var index = 2; index < registerInputs.length; index++) {
+    registerInputs[index].oninvalid = function(e){
+      e.target.setCustomValidity("");
+      if (!e.target.validity.valid) {
+        e.target.setCustomValidity("Campos inválidos");
+      }
+    }
+    registerInputs[index].oninput = function(e) {
+      e.target.setCustomValidity("");
+    };
+  };
+}
+
+
+document.addEventListener('DOMContentLoaded', errorMessage)
 entrarButton.addEventListener('click', alertButton);
