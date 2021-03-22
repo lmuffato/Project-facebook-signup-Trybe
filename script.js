20e414fe228579832594ca1e95606a2bfa643f92
Patrick Morais <ppternunes@gmail.com>
2021-03-22 15:38:58 -0300

    requisito 9 - 10 e e estilizações

---
+++ script.js
@@ -0,0 +1,12 @@
+function alertEmail() {
+  const btnLogin = document.getElementById('button-login')
+
+  btnLogin.addEventListener('click', () => {
+    const emailValue = document.getElementById('user-email-phone').value
+    alert(`${emailValue}`)
+  })
+}
+
+window.onload = () => {
+  alertEmail()
+}
