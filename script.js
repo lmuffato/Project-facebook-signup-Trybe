a38e003d9898a854d5542f035b096120cdcd0163
rcalvs <rafaelcalvette@gmail.com>
2021-03-22 16:26:38 -0300

    Requisito 18 - Inicio

---
+++ script.js
@@ -0,0 +1,33 @@
+// Consts //
+const buttonFacebookRegister = document.querySelector('#facebook-register');
+let submitCount = 0;
+
+
+
+// function validateRegister {
+//     // First Name
+//     // Last Name
+//     // Phone/email
+//     // Password
+//     // Bitrh
+//     // Gender
+// }
+
+// Funções de Validação //
+// https://www.w3schools.com/jsref/coll_doc_forms.asp //
+function validateFirstName() {
+    if (document.forms[namedItem(id)].value === '') {
+
+    } else {
+        submitCount += 1;
+    }
+}
+
+Console.log(submitCount)
+
+
+
+
+buttonRegister.addEventListener('click', validateFirstName)
+//Eventos //
+// buttonRegister.addEventListener('click', validateRegister); // Requisito 18, puxa validateRegister e essa puxa as outras validações //
