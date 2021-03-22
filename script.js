3af8d570e48782e1f995915a411f331b67702b73
Paulo H. Lopes <phlopes93@gmail.com>
2021-03-22 20:59:15 -0300

    correction attempt requirement to 18

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const selectButton = document.querySelector('#button-login');
 const selectInput = document.querySelector('#user-email-phone');
-const sendForm = document.querySelector('#facebook-register');
-const selectInputs = document.querySelectorAll('.inputs, .inputs2');
-const formSignUp = document.querySelector('#form-signup');
+// const sendForm = document.querySelector('#facebook-register');
+// const selectInputs = document.querySelectorAll('.inputs, .inputs2, .inputRadio');
+// const formSignUp = document.querySelector('#form-signup');
 // const rightContent = document.querySelector('.right-content');
 // const signupForm = document.querySelector('#signup-form');
+// const divMensagem = document.querySelector('#mensagem');
 
 function alertInput() {
   alert(selectInput.value);
 }
-
-function checkInputs() {
-  for (let index = 0; index < selectInputs.length; index += 1) {
-    if (selectInputs[index].value === '') {
-      const creatP = document.createElement('p');
-      creatP.innerHTML = 'Campos inválidos';
-      formSignUp.appendChild(creatP);
-    }
-  }
-}
+// function checkInputs() {
+//   let mensagem = '';
+//   for (let index = 0; index < selectInputs.length; index += 1) {
+//     const element = selectInputs[index];
+//     if (element.value === '') {
+//       mensagem = 'Campos inválidos';
+//     } if (element.type === 'radio' && !element.checked) {
+//       mensagem = 'Campos inválidos';
+//     }
+//   }
+//   divMensagem.innerHTML = mensagem;
+// }
 
 selectButton.addEventListener('click', alertInput);
-sendForm.addEventListener('click', checkInputs);
+// sendForm.addEventListener('click', checkInputs);
