b0ed57c01b96602c02bf0c8514101db5f5dbba4b
Felipe Muller <felipeavmuller@gmail.com>
2021-03-22 20:26:37 -0300

    Requisito 19

--- script.js
+++ script.js
@@ -7,6 +7,32 @@ function loginAlert() {
   });
 }
 
+function addCustonGender() {
+  const gendersBlock = document.getElementById('genders');
+  gendersBlock.addEventListener('click', (event) => {
+    const gendersLength = gendersBlock.children.length;
+    if (event.target.id === 'gen-personalized' && gendersLength < 7) {
+      const newGender = document.createElement('input');
+      newGender.name = 'gender-custom';
+      newGender.placeholder = 'Gênero (opcional)';
+      gendersBlock.appendChild(newGender);
+    }
+  });
+}
+
+function removeCustonGender() {
+  const gendersBlock = document.getElementById('genders');
+  gendersBlock.addEventListener('click', (event) => {
+    const gendersLength = gendersBlock.children.length;
+    const idE = event.target.id;
+    if ((idE === 'gen-masc' || idE === 'gen-fem') && gendersLength > 6) {
+      gendersBlock.removeChild(gendersBlock.lastChild);
+    }
+  });
+}
+
 window.onload = function run() {
   loginAlert();
+  addCustonGender();
+  removeCustonGender();
 };
