c87fa5578dba88b31ac86aacb80dc3f2b190d146
José Luiz Fagundes <jlfagundes@gmail.com>
2021-03-22 17:55:24 -0300

    lint requisitos 3 ao 7

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('#button-login').addEventListener(click, function () {
-    const phoneOrEmail = document.querySelector('#user-email-phone').value;
-    alert(phoneOrEmail);
+document.querySelector('#button-login').addEventListener('click', function () {
+  const phoneOrEmail = document.querySelector('#user-email-phone').value;
+  alert(phoneOrEmail);
 });
