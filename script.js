0c2c382adeb90b737f1a47135e30d1f9c3ac8a75
Gabriel Ferreira <glucas709@gmail.com>
2021-03-22 19:34:20 -0300

    Revisnado requisito 7 de novo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btnLogin = document.getElementById('button-login');
-const emailPhone = document.getElementById('user-email-phone');
+const emailPhone = document.getElementById('user-email-phone').value;
 
 btnLogin.addEventListener('click', () => {
-  alert(emailPhone.value);
+  alert(emailPhone);
 });
