26656b36221bc3816de0c699c51d2224aa39f4ee
Vinicius Rodrigues <V.rodriguesjk@gmail.com>
2021-03-22 20:35:57 -0300

    Concerta link entre script e html

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const botaoEntrar = document.getElementById('button-login');
 
 function alerta() {
-  console.log('estou aqui');
   const email = document.getElementById('user-email-phone').value;
   if (email !== undefined) {
     alert(email);
   }
 }
-botaoEntrar.addEventListener('click', alerta);
+botaoEntrar.addEventListener('click', () => {
+  alerta();
+});
