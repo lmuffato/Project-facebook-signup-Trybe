043ec3361bb03f0c7965828406c1ea72914327f9
Jodiel Briesemeister <jodielll29@gmail.com>
2021-03-22 20:49:39 -0300

    requisito 15

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //  Função click exibe alerta com valor do campo
 function testFunction() {
-  // alert(document.getElementById('user-email-phone').value);
   const button = document.querySelector('.form-control');
   button.addEventListener('click', () => {
     const text = document.getElementById('user-email-phone').value;
