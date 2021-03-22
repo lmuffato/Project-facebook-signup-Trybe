2d8969bc63060e830700a53d1614ae45c050730e
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-03-22 19:05:31 -0300

    requisito 18 linter fixed

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 const entrarButton = document.getElementById('button-login');
 
-
 function alertButton() {
   const valorEmail = document.getElementById('user-email-phone').value;
   alert(valorEmail);
 }
 
-
-//codigo encontrado no site https://stackoverflow.com/questions/5272433/html5-form-required-attribute-set-custom-validation-message 
+//  codigo encontrado no site https://stackoverflow.com/questions/5272433/html5-form-required-attribute-set-custom-validation-message
 // Author: robertc
 
 function errorMessage() {
-  const registerInputs = document.getElementsByTagName('input')
-  for (var index = 2; index < registerInputs.length; index++) {
-    registerInputs[index].oninvalid = function(e){
-      e.target.setCustomValidity("");
+  const registerInputs = document.getElementsByTagName('input');
+  for (let index = 2; index < registerInputs.length; index += 1) {
+    registerInputs[index].oninvalid = function b(e) {
+      e.target.setCustomValidity('');
       if (!e.target.validity.valid) {
-        e.target.setCustomValidity("Campos inválidos");
+        e.target.setCustomValidity('Campos inválidos');
       }
-    }
-    registerInputs[index].oninput = function(e) {
-      e.target.setCustomValidity("");
     };
-  };
+    registerInputs[index].oninput = function a(e) {
+      e.target.setCustomValidity('');
+    };
+  }
 }
 
-
-document.addEventListener('DOMContentLoaded', errorMessage)
+document.addEventListener('DOMContentLoaded', errorMessage);
 entrarButton.addEventListener('click', alertButton);
