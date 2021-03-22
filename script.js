7f6bb5d18fee7756b8b1939456ee191b8a20e61c
Gui-lira <gui.lira.martins@gmail.com>
2021-03-22 19:26:40 -0300

    :rocket: 18

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function alertLogin() {
 
 const btnLogin = document.getElementById('button-login');
 btnLogin.addEventListener('click', alertLogin);
+
+function validateForm() {
+  const list = document.querySelectorAll('input');
+  for (let index = 0; index < list.length; index += 1) {
+    if (list[index].value === '') {
+      alert('Campos inválidos');
+    }
+  }
+}
+
+const buttonRegister = document.querySelector('#facebook-register');
+
+buttonRegister.addEventListener('click', validateForm);
