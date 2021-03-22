cd4b7b98fbb590695823cdf52a47261116d03936
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-03-22 19:18:29 -0300

    requesito 7

---
+++ script.js
@@ -0,0 +1,7 @@
+// add alert no button login
+const loginButton = document.getElementById('button-login');
+function buttonAlert() {
+  const camp = document.getElementById('user-email-phone').value;
+  alert(camp);
+}
+loginButton.addEventListener('click',buttonAlert);
