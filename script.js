d9205b43821477cf26e01811730c8cebc6081b12
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-03-22 18:32:16 -0300

    Enviando requisitos 9 e 10

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let button = document.getElementById("button-login");
-button.addEventListener("click", function(event){
+const button = document.getElementById('button-login');
+button.addEventListener('click', submitCancel);
+function submitCancel(event) {
   event.preventDefault();
   const login = document.querySelector('#user-email-phone').value;
   alert(`${login}`);
