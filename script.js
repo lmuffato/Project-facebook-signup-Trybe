07d81a1c5c907593e599c947452c788761c8f87d
Matheus Lima Pimenta <mateus.krauss@gmail.com>
2021-03-22 18:28:50 -0300

    Corrigindo lint

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 window.onload = function () {
-    
-    let btnLogin = document.getElementById('button-login');
-    btnLogin.addEventListener('click', () => {
-        let inputName = document.getElementById('user-email-phone-label').value;
-        let inputPw = document.getElementById('user-password-label').value;
-        alert('Login: ' + inputName + ' senha ' + inputPw) 
+    const btnLogin = document.getElementById('button-login');
+  btnLogin.addEventListener('click', () => {
+  const inputName = document.getElementById('user-email-phone-label').value;
+  const inputPw = document.getElementById('user-password-label').value;
+    alert('Login: ' + inputName + ' senha ' + inputPw);
     })
 }
