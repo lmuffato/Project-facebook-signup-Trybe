a8e940de131fe9861742b8db1f96e09498932f55
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-03-22 19:18:36 -0300

    Requisito 7 corrigido - Mudanças no JS

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function enterButton() {
   const emailOrCel = document.getElementById('user-email-phone').value;
   alert(emailOrCel);
 }
+
+const button = document.getElementById('button-login');
+button.addEventListener('click', enterButton);
