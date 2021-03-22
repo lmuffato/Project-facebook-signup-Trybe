7b0cbc0b14dc1efa5f4e9dc002a3efdeba44b4bd
Bruno Mendes <mendessbruno@gmail.com>
2021-03-22 17:12:25 -0300

    resolvidos os problemas do avaliador (classes nos formularios de login e txto exibido no alert)

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function loginAlert() {
   const btnLogin = document.getElementById('button-login');
+  let inputText = '';
   btnLogin.addEventListener('click', () => {
-    alert('Email ou telefone');
+    inputText = document.getElementById('user-email-phone').value;
+    alert(inputText);
   });
 }
 
