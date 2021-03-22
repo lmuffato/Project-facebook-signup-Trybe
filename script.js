11e047e75bf3e01df80fc2d6e94efabce4ee50a6
andy silva <andssilva.23@gmail.com>
2021-03-22 15:38:55 -0400

    requisito 7, correção 2, 3, 5, 6

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttonLogin = document.getElementById('button-login');
+
+function alertLoginButton() {
+  alert('Email ou telefone');
+}
+
+buttonLogin.addEventListener('click', alertLoginButton);
