871521b2df18e0e4d44ebcdfe30e044457a33db9
Felipe Muller <felipeavmuller@gmail.com>
2021-03-22 20:46:09 -0300

    requisito 19

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function alert(number) {
+  if (number !== 5) {
+    document.querySelector('.alert').style.display = 'block';
+  }
+}
+
 function loginAlert() {
   const btnLogin = document.getElementById('button-login');
   let inputText = '';
@@ -31,8 +37,38 @@ function removeCustonGender() {
   });
 }
 
+function getElements() {
+  const name = document.getElementById('firsname').value;
+  const lastName = document.getElementById('lastname').value;
+  const phoneEmail = document.getElementById('phone-email').value;
+  const password = document.getElementById('password').value;
+  const birthdate = document.getElementById('birthdate').value;
+  const result = [name, lastName, phoneEmail, password, birthdate];
+  return result;
+}
+
+function check() {
+  const result = getElements();
+  let sum = 0;
+  for (let index = 0; index < result.length; index += 1) {
+    const element = result[index];
+    if (element !== '') {
+      sum += 1;
+    }
+  }
+  return sum;
+}
+
+function validate() {
+  const btn = document.getElementById('facebook-register');
+  btn.addEventListener('click', () => {
+    alert(check());
+  });
+}
+
 window.onload = function run() {
   loginAlert();
   addCustonGender();
   removeCustonGender();
+  validate();
 };
