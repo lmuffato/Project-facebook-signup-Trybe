c9c669298ac28dbd159e1973c3e4a90f997996ab
diegoCorreia <diegomethal@gmail.com>
2021-03-22 18:37:23 -0300

    requisitos 6 e 7

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const inputLogin = document.getElementById('user-email-phone');
 const buttonLogin = document.getElementById('button-login');
-function alertPasswordEvent(){
-    alert(inputLogin.value);
+function alertPasswordEvent() {
+  alert(inputLogin.value);
 }
 function alertPassword() {
-   buttonLogin.addEventListener('click', alertPasswordEvent)
+  buttonLogin.addEventListener('click', alertPasswordEvent)
 }
 alertPassword();
+
