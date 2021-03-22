583a5ff145af5bbdaf7f776caa3a019178f31f40
defauth98 <neto.daniribeiro@gmail.com>
2021-03-22 16:32:13 -0300

    Requisito 19

--- script.js
+++ script.js
@@ -7,18 +7,33 @@ function buttonAlert() {
 function inputValidate() {
   const inputs = document.querySelectorAll('.main-content input');
 
-  for(let index = 0; index < inputs.length; index += 1) {
-    if(inputs[index].value === '') {
+  for (let index = 0; index < inputs.length; index += 1) {
+    if (inputs[index].value === '') {
       alert('Campos inválidos');
       return;
     }
   }
 }
 
+function showCustomGenderOptions(input) {
+  const containerElement = document.getElementById('custom-gender-container');
+
+  if (input.id === 'custom-gender' && input.checked) {
+    containerElement.style.display = 'flex';
+  } else {
+    containerElement.style.display = 'none';
+  }
+}
+
 window.onload = () => {
   const buttonLoginElement = document.getElementById('button-login');
   const buttonSubmitElement = document.getElementById('facebook-register');
+  const inputs = document.querySelectorAll('#inputs-container input');
 
   buttonLoginElement.onclick = buttonAlert;
   buttonSubmitElement.onclick = inputValidate;
+
+  for (let index = 0; index < inputs.length; index += 1) {
+    inputs[index].onclick = () => showCustomGenderOptions(inputs[index]);
+  }
 };
