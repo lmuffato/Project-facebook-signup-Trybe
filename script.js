588dedb2ada0690b05ba2fc59fbefe99ae7b245e
Gui-lira <gui.lira.martins@gmail.com>
2021-03-22 20:44:13 -0300

    :rocket: 18

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ function alertLogin() {
 const btnLogin = document.getElementById('button-login');
 btnLogin.addEventListener('click', alertLogin);
 
+const alertText = document.createElement('p');
+alertText.innerText = 'Campos inválidos'
+
 function validateForm() {
   const list = document.querySelectorAll('input');
   for (let index = 0; index < list.length; index += 1) {
     if (list[index].value === '') {
-      alert('Campos inválidos');
+      document.querySelector('#formMain').appendChild(alertText)
     }
   }
 }
