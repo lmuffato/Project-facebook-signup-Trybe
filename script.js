114a39d44a16a6f433d7d2f9aa9f638c40ee040e
jonathan <jonathanfernandesjf98@gmail.com>
2021-03-22 17:11:02 -0300

    Requisito 07, adicao do subcontainer com botao de entrar, quando clicado dispara um alert com o valor do campo email

---
+++ script.js
@@ -0,0 +1,10 @@
+function alertFunction() {
+  const input = document.getElementById('user-email-phone')
+  alert(input.value)
+}
+
+function alertButtonLogin() {
+  const buttonLogin = document.getElementById('button-login')
+  buttonLogin.addEventListener('click', alertFunction)
+}
+alertButtonLogin()
