4b5e8a164dde1660c0517b3253f0cdba26993b3e
biazidioti <bzferreira.bio@gmail.com>
2021-03-22 18:52:06 -0300

    Arruma lint.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function alert1() {
-  alert ('Email ou telefone');
+  alert('Email ou telefone');
 }
 const buttonLogin = document.querySelector('#button-login');
 buttonLogin.addEventListener('click', alert1);
