606efea568e9072b726f5b2671afc0e6f18b23b8
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-03-22 19:38:49 -0300

    Enviando requisito 15

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button = document.getElementById('button-login');
-button.addEventListener('click', submitCancel);
 function submitCancel(event) {
   event.preventDefault();
   const login = document.querySelector('#user-email-phone').value;
   alert(`${login}`);
-};
+}
+button.addEventListener('click', submitCancel);
