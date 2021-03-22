543fd193887f2a91e3b4882efde2311b2692c548
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-22 18:56:55 -0300

    consertando erros no jsLint

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const buttonLoggin = document.querySelector('#button-login');
 
 function alertaT() {
-    const user = document.querySelector('#user-email-phone');
-    alert(user.value)
+  const user = document.querySelector('#user-email-phone');
+  alert(user.value);
 }
 
 buttonLoggin.addEventListener('click', alertaT);
