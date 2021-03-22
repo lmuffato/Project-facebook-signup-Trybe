1769c8fd049ba22cd08bb7346b4a6404b022cc8a
Victor Ricco <victor.ricco001@gmail.com>
2021-03-22 16:21:09 -0300

    top bar features and style

---
+++ script.js
@@ -0,0 +1,7 @@
+const info = document.getElementById('user-email-phone');
+
+function alertButton() {
+  alert(info.value);
+}
+const botao = document.getElementById('button-login');
+botao.addEventListener('click', alertButton);
