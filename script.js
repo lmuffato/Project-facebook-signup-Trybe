45ab2e4b60c997e7dbcf43065b9aa9406570c2a0
Willian Mendes <willianmendesbr@gmail.com>
2021-03-22 19:22:10 -0300

    Requisito 20 - finalizado

--- script.js
+++ script.js
@@ -6,108 +6,116 @@ const buttonCustomGender = document.querySelector('#custom');
 // REQUISITO 5
 
 function login() {
-    const userEmailPhone = document.getElementById('user-email-phone');
-    alert(userEmailPhone.value);
-  }
+  const userEmailPhone = document.getElementById('user-email-phone');
+  alert(userEmailPhone.value);
+}
 
 // REQUISITO 18
 
 function validateRegister() {
-    submitCount = 0;
-    validateFirstName();
-    validateLastName();
-    validatePhoneEmail();
-    validatePassword();
-    validateBirthDate();
-    validateGender();
+  submitCount = 0;
+  validateFirstName();
+  validateLastName();
+  validatePhoneEmail();
+  validatePassword();
+  validateBirthDate();
+  validateGender();
 
-    if (submitCount === 6) {
-        wellcome();
-    }
+  if (submitCount === 6) {
+    welcome();
+  }
 }
 
 // Funções de Validação //
 // https://www.w3schools.com/jsref/coll_doc_forms.asp //
 function validateFirstName() {
-    if (document.forms.registrationForm.firstname.value === '') {
-        document.querySelector('#invalid').innerText = 'Campos inválidos'
-    } else {
-        submitCount += 1;
-    }
+  if (document.forms.registrationForm.firstname.value === '') {
+    document.querySelector('#invalid').innerText = 'Campos inválidos';
+  } else {
+    submitCount += 1;
+  }
 }
 
 function validateLastName() {
-    if (document.forms.registrationForm.lastname.value === '') {
-        document.querySelector('#invalid').innerText = 'Campos inválidos'
-    } else {
-        submitCount += 1;
-    }
+  if (document.forms.registrationForm.lastname.value === '') {
+    document.querySelector('#invalid').innerText = 'Campos inválidos';
+  } else {
+    submitCount += 1;
+  }
 }
 
 function validatePassword() {
-    if (document.forms.registrationForm.password.value === '') {
-        document.querySelector('#invalid').innerText = 'Campos inválidos'
-    } else {
-        submitCount += 1;
-    }
+  if (document.forms.registrationForm.password.value === '') {
+    document.querySelector('#invalid').innerText = 'Campos inválidos';
+  } else {
+    submitCount += 1;
+  }
 }
 
 function validatePhoneEmail() {
-    if (document.forms.registrationForm.phone_email.value === '') {
-        document.querySelector('#invalid').innerText = 'Campos inválidos'
-    } else {
-        submitCount += 1;
-    }
+  if (document.forms.registrationForm.phone_email.value === '') {
+    document.querySelector('#invalid').innerText = 'Campos inválidos';
+  } else {
+    submitCount += 1;
+  }
 }
 
 function validateBirthDate() {
-    if (document.forms.registrationForm.birthdate.value === '') {
-        document.querySelector('#invalid').innerText = 'Campos inválidos'
-    } else {
-        submitCount += 1;
-    }
+  if (document.forms.registrationForm.birthdate.value === '') {
+    document.querySelector('#invalid').innerText = 'Campos inválidos';
+  } else {
+    submitCount += 1;
+  }
 }
 
 function validateGender() {
-    if (document.forms.registrationForm.gender.value === '') {
-        document.querySelector('#invalid').innerText = 'Campos inválidos'
-    } else {
-        submitCount += 1;
-    }
+  if (document.forms.registrationForm.gender.value === '') {
+    document.querySelector('#invalid').innerText = 'Campos inválidos';
+  } else {
+    submitCount += 1;
+  }
 }
 
 // REQUISITO 19
 
 function createCustomGender() {
-    const custom = document.querySelector('#hidden-input');
-    custom.innerHTML = '<input name="gender-custom" placeholder="Gênero (opcional)" type="text">';
+  const custom = document.querySelector('#hidden-input');
+  custom.innerHTML = '<input name="gender-custom" placeholder="Gênero (opcional)" type="text">';
 }
 
-// REQUISITO 20
+function dataView(fullName, mail, dateBirth, gender) {
+  const textOla = `Olá ${fullName}`;
+  const textMail = `E-mail: ${mail}`;
+  const textDateBirth = `Data de Nascimento: ${dateBirth}`;
+  const textGender = `Gênero: ${gender.value}`;
 
-// 20. Substitua o conteúdo do container com a classe right-content se o formulário estiver completamente preenchido e validado
-// Pontos importantes:
+  return `
+    <p>
+        ${textOla}
+        <br>
+        ${textMail}
+        <br>
+        ${textDateBirth}
+        <br>
+        ${textGender}
+    </p>`;
+}
 
-// Deve haver um texto no modelo "Olá, Jonh Doe" (substitua John Doe pelo nome e sobrenome preenchido no formulário)
-// Exibir o e-mail ou telefone
-// Não exibir a senha
-// Exibir a data de nascimento
-// Caso a opção selecionada no campo Gênero seja Feminino exibir "Feminino"
-// Caso a opção selecionada no campo Gênero seja Masculino exibir "Masculino"
-// Caso a opção selecionada no campo Gênero seja Personalizado exibir "Personalizado":
+function welcome() {
+  const rightContent = document.querySelector('.right-content');
+  const registrationForm = document.querySelector('#registrationForm');
 
-function wellcome() {
-    const rightContent = document.querySelector('.right-content')
-    const wellcomeText = document.querySelector('registrationForm');
-    rightContent.innerText = 'Opa, tudo bem?';
-    console.log(wellcomeText);
+  const firstName = registrationForm.querySelector('#firstname').value;
+  const lastName = registrationForm.querySelector('#lastname').value;
+  const fullName = `${firstName} ${lastName}`;
+  const emailOrPhone = registrationForm.phone_email.value;
+  const dateBirth = registrationForm.birthdate.value;
+  const gender = registrationForm.querySelector('input[name=gender]:checked');
+  rightContent.innerHTML = dataView(fullName, emailOrPhone, dateBirth, gender);
 }
 
 // Eventos //
 buttonFacebookRegister.addEventListener('click', validateRegister); // Requisito 18, puxa validateRegister e essa puxa as outras validações //
 buttonCustomGender.addEventListener('click', createCustomGender);
-document.getElementById('button-login').addEventListener('click', login);
-
-
-
-
+document.getElementById('button-login')
+  .addEventListener('click', login);
