865fcec7f6802862816b6695d83615be30969566
Guilherme de Prais <guilherme.prais@gmail.com>
2021-03-22 20:40:40 -0300

    requirement 20

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ function validateForm(event) {
     createParagraph.innerText = 'Campos inválidos';
     accForm.appendChild(createParagraph);
     event.preventDefault();
+  } else {
+    event.preventDefault();
+    rightContentForm();
   }
 }
 
@@ -36,14 +39,59 @@ btn.addEventListener('click', alertLogin);
 // Criar campo personalizado
 const genderForm = document.querySelector('#gender-form');
 
-function genderCustom(event) {
-  const createInput = document.createElement('input');
-  createInput.setAttribute('name', 'gender-custom');
-  createInput.setAttribute('placeholder', 'Gênero (opcional)');
+// function genderCustom(event) {
+//   const createInput = document.createElement('input');
+//   createInput.setAttribute('name', 'gender-custom');
+//   createInput.setAttribute('placeholder', 'Gênero (opcional)');
 
-  genderForm.appendChild(createInput);
+//   genderForm.appendChild(createInput);
+// }
+
+// const btnCustom = document.querySelector('#custom');
+
+// btnCustom.addEventListener('change', genderCustom);
+
+// Substituir conteúdo do right-content
+const rightContent = document.querySelector('.right-content');
+const nameInput = document.querySelector('#name');
+const lastName = document.querySelector('#lastname');
+const emailInput = document.querySelector('#email');
+const birthDate = document.querySelector('#birthdate');
+const radios = document.querySelectorAll('.radio');
+const genderRadio = document.querySelector('#custom');
+
+function rightContentForm() {
+  let genderInput = '';
+  for (let i = 0; i < radios.length; i += 1) {
+    if (radios[i].classList.contains('selected')) {
+      genderInput = radios[i].value;
+    }
+
+    rightContent.innerText = `Olá, ${nameInput.value} ${lastName.value}
+  
+  ${emailInput.value}
+
+  ${birthDate.value}
+
+  ${genderInput}
+  `;
+  }
+}
+const customGender = document.querySelector('#custom-gender');
+
+for (let i = 0; i < radios.length; i += 1) {
+  radios[i].addEventListener('change', customGenderForm);
 }
 
-const btnCustom = document.querySelector('#custom');
+function customGenderForm(event) {
+  for (let radiosIndex = 0; radiosIndex < radios.length; radiosIndex += 1) {
+    radios[radiosIndex].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
 
-btnCustom.addEventListener('change', genderCustom);
+  if (genderRadio.classList.contains('selected')) {
+    customGender.style.display = 'inherit';
+  } else {
+    customGender.style.display = 'none';
+  }
+}
