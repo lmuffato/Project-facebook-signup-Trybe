a612ffd6c35c47cbc753f2c98dbda9960efefd67
Debora Passos <deborinhapassos@gmail.com>
2021-03-22 21:30:32 -0300

    requisito 7

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('#button-login');
+const contact = document.querySelector('#user-email-phone'); 
+
+function getContact() {
+  alert(contact.value);
+}
+
+btn.addEventListener('click', getContact);
