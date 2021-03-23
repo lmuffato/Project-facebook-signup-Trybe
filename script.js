a9ab4a6ac5765e6a9ba076e0926551a26d4f9944
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-03-22 21:47:44 -0300

    requesito 19

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ function buttonAlert() {
   alert(camp);
 }
 loginButton.addEventListener('click', buttonAlert);
+
+// add o personalize option gender 
+const toBe = document.getElementById('to-be');
+const toBeInput = document.createElement('input');
+toBeInput.id = 'to-be-input';
+toBeInput.name = 'gender-custom';
+toBeInput.placeholder = 'Gênero (opcional)';
+console.log(toBe)
+function newCamp() {
+  if(toBe.checked === true) {
+    document.getElementById('gender-input-if-true').appendChild(toBeInput);
+  }
+}
+function removeCamp() {
+  if(toBe.checked === false) {
+    document.getElementById('gender-input-if-true').removeChild(toBeInput);
+  }
+}
+const male = document.getElementById('male');
+const female = document.getElementById('female')
+male.addEventListener('click', removeCamp);
+female.addEventListener('click', removeCamp);
+toBe.addEventListener('click', newCamp);
+
