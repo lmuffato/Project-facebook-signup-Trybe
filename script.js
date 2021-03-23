b5a632393cd44683e488dda1983bc1a7351c5d0f
Anibal <anibalneto29@gmail.com>
2021-03-23 00:34:15 -0300

    requisitos 8 e 9

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function alertaT() {
 }
 
 buttonLoggin.addEventListener('click', alertaT);
+
+Adição de frase referente ao requisito 8 no left-container
+
+function addParagraph() {
+  let text = document.getElementsByTagName('p')[0].innerText('O Facebook ajuda você a se conectar e compartilhar com as pessoas que fazem parte da sua vida.');
+  return text;
+}
+
+addParagraph();
