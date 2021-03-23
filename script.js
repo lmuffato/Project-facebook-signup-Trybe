7bd37a1f3493749a08eb029ab02840a7216846b5
Ana Ventura <anaventura1811@gmail.com>
2021-03-23 11:17:34 -0300

    Adiciona função hideField e adiciona requisito 20

--- script.js
+++ script.js
@@ -4,16 +4,15 @@ btnLogin.addEventListener('click', () => {
   alert(emailOuTelefone.value);
 });
 
-//  Exibir uma mensagem "Campos inválidos" dentro do formulário caso pelo menos um campo não esteja preenchido
+// Requisito 18 - Exibir uma mensagem "Campos inválidos" dentro do formulário caso pelo menos um campo não esteja preenchido
 const facebookForm = document.getElementById('facebook-form');
 const span = document.createElement('span');
 function validatingForm(event) {
-  for (let key = 0; key < facebookForm.elements.length; key += 1) {
+  for (let key = 0; key < 7; key += 1) {
     if (facebookForm.elements[key].value === '') {
       event.preventDefault();
       span.innerHTML = 'Campos inválidos';
       facebookForm.appendChild(span);
-      console.log('estou funfando');
     }
   }
 }
@@ -21,15 +20,66 @@ function validatingForm(event) {
 const btnSubmit = document.getElementById('facebook-register');
 btnSubmit.addEventListener('click', validatingForm);
 
-<<<<<<< HEAD
-/* Substitua o conteúdo do container com a classe right-content se o formulário
-estiver completamente preenchido e validado */
-// Faltam os requisitos 19 e 20.
-=======
+// Requisito 19
+const genderField = document.getElementById('gender-custom');
 function createField() {
-  const genderField = document.getElementById('gender-custom');
   genderField.style.display = 'flex';
 }
 
-document.addEventListener('click', createField);
->>>>>>> 833af0c6f31d2a4f8884661a6c6a0ccef109c6fb
+const custom = document.getElementById('custom');
+custom.addEventListener('click', createField);
+
+const male = document.getElementById('male');
+const female = document.getElementById('female');
+
+// Tive que incluir a função hideField, porque, ao clicar em qualquer um dos radio buttons, o display do
+// gender-custom tava indo pra flex.
+
+function hideField() {
+  genderField.style.display = 'none';
+}
+male.addEventListener('click', hideField);
+female.addEventListener('click', hideField);
+
+/* Substitua o conteúdo do container com a classe right-content se o formulário
+estiver completamente preenchido e validado */
+
+const firstName = document.getElementById('firstname');
+const lastName = document.getElementById('lastname');
+const celEmail = document.getElementById('celOuEmail');
+const birth = document.getElementById('birthdate');
+
+function gender() {
+  if (female.checked) {
+    return female.value;
+  }
+  if (male.checked) {
+    return male.value;
+  }
+  if (custom.checked) {
+    return custom.value;
+  }
+}
+function revalidatingForm() {
+  if (firstName.value !== '' && lastName.value !== '') {
+    return true;
+  }
+  if (celEmail.value !== '' && birth.value !== '') {
+    return true;
+  }
+  return false;
+}
+
+function helloMessage(e) {
+  const rightContent = document.querySelector('.right-content');
+  e.preventDefault();
+  if (revalidatingForm() === true) {
+    rightContent.innerHTML = '';
+    rightContent.innerHTML = `<p>Olá, ${firstName.value} ${lastName.value}</p>`;
+    rightContent.innerHTML += `<br><p>${celEmail.value}</p>`;
+    rightContent.innerHTML += `<br><p>${birth.value}</p>`;
+    rightContent.innerHTML += `<br><p>${gender()}`;
+  }
+}
+
+btnSubmit.addEventListener('click', helloMessage);
