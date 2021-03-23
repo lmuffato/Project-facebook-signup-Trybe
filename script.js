95efad6c8419724084365d319a9cf17041faef98
Carol <carolinavpinheiro@gmail.com>
2021-03-23 13:50:24 -0300

    Adicionando alert com email ou telefone

---
+++ script.js
@@ -0,0 +1,6 @@
+const buttonLogin = document.querySelector('#button-login');
+const emailFone = document.querySelector('#user-email-phone');
+
+buttonLogin.addEventListener('click', () => {
+  window.alert(emailFone.value);
+})
