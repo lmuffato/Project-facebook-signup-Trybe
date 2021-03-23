1a76aece54777dc6b7c26c6570c56e8042a1be27
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-03-22 23:54:24 -0300

    consertos ate o exe 6

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function buttonLogin() {
   const takeButton = document.querySelector('#button-login');
   takeButton.addEventListener('click', () => {
-    const takeLogin = document.querySelector('#user-email-phone').innerHTML;
+    const takeLogin = document.querySelector('input#user-email-phone').value;
     alert(takeLogin);
   });
 }
