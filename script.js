8afb34a6893c212edd18a87fbdacad64d09ca728
Alexandre Oliveira <alexandremolive@gmail.com>
2021-03-23 10:51:23 -0300

    exercicio 18

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function alertEmail() {
 }
 
 btnLogin.addEventListener('click', alertEmail);
+
+const btnRegister = document.querySelector('#facebook-register');
+function checkInputs() {
+  const userInputs = document.querySelectorAll('input');
+  for (let index = 0; index < userInputs.length; index += 1) {
+    if (userInputs[index].value === '') {
+      alert('Campos inválidos');
+      break;
+    }
+  }
+}
+btnRegister.addEventListener('click', checkInputs);
