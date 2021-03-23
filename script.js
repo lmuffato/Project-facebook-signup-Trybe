1c257f7555af50e06db1a3c1704e4a0d5a86f270
Anibal <anibalneto29@gmail.com>
2021-03-23 00:43:58 -0300

    tentativa de disparar o evaluator

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-/* function button login */
 const buttonLoggin = document.querySelector('#button-login');
 
-function alertaT(e) {
-  e.preventDefault();
+function alertaT() {
   const user = document.querySelector('#user-email-phone');
   alert(user.value);
 }
 
 buttonLoggin.addEventListener('click', alertaT);
 
-Adição de frase referente ao requisito 8 no left-container
-
-function addParagraph() {
-  let text = document.getElementsByTagName('p')[0].innerText('O Facebook ajuda você a se conectar e compartilhar com as pessoas que fazem parte da sua vida.');
-  return text;
-}
-
-addParagraph();
