2132ea8c722390c283b49e1629a250684315d757
lucas-paz <lucas.hpaz@outlook.com>
2021-03-23 10:49:36 -0300

    Adiciona requisito 19

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function requiredCamp(e) {
   const inputs = document.querySelectorAll('.right-content input');
   for (let index = 0; index < inputs.length; index += 1) {
     if (inputs[index].value === '') {
-      let msg = 'Campos inválidos'
+      const msg = 'Campos inválidos';
       inputs[index].value = msg;
     }
   }
@@ -20,3 +20,17 @@ function requiredCamp(e) {
 
 const buttonVerifier = document.getElementById('facebook-register');
 buttonVerifier.addEventListener('click', requiredCamp);
+
+function addNewGender() {
+  const newGender = document.querySelector('#personalizado');
+  const newUser = document.querySelector('#addNewGender');
+  if (newGender.checked === true) {
+    const add = document.createElement('input');
+    add.placeholder = 'Gênero (opcional)';
+    add.name = 'gender-custom';
+    const print = newUser.appendChild(add);
+  }
+}
+
+const person = document.getElementById('personalizado');
+person.addEventListener('click', addNewGender);
