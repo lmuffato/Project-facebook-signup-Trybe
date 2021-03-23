2d6657daa2ea0a6efe1936ae740ff7f041ef4794
Lucas Muffato <lmiremuffato@gmail.com>
2021-03-22 22:45:38 -0300

    atualização

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 window.onload = () => {
-<<<<<<< HEAD
+HEAD
     document.querySelector('#button-login').addEventListener('click', () => {
       alert(document.querySelector('#user-email-phone').value);
     });
   }
-=======
-  document.querySelector('#button-login').addEventListener('click', () => {
-    alert(document.querySelector('#user-email-phone').value);
-  });
-};
->>>>>>> 2cb85981abf73e8729392f99f344acea4329fd50
+
