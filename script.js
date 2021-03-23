4b41469ed171e882350ddaf618d7a9592f7a571e
diogomarin <diogoamarin@gmail.com>
2021-03-23 12:14:06 -0300

    Meeting requirement 18

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
+const buttonSubmit = document.getElementById('button-login');
+const buttonRegister = document.getElementById('facebook-register');
+const radiosButtons = document.getElementsByName('gender');
+const registerInputs = document.querySelectorAll('.register');
+// const generoOpcional = document.querySelector('#genero-opcional');
+
 function submit() {
   alert(document.getElementById('user-email-phone').value);
 }
 
-const buttonSubmit = document.getElementById('button-login');
+function checkRegister() {
+  let confirmRegister = true;
+
+  for (let i = 0; i < registerInputs.length; i += 1) {
+    if (registerInputs[i].value === '') {
+      confirmRegister = false;
+    }
+  }
+  return confirmRegister;
+}
+
+function checkRadios() {
+  let confirmRadios = false;
+
+  for (let j = 0; j < radiosButtons.length; j += 1) {
+    if (radiosButtons[j].checked === true) {
+      confirmRadios = true;
+    }
+  }
+  return confirmRadios;
+}
+
+function checkData() {
+  if (checkRegister() === false || checkRadios() === false) {
+    alert('Campos Inválidos');
+  }
+}
+
 buttonSubmit.addEventListener('click', submit);
+buttonRegister.addEventListener('click', checkData);
