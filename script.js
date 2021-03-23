fe9178392c786f34e773307161b9aaea941c3728
lucas-paz <lucas.hpaz@outlook.com>
2021-03-22 22:09:13 -0300

    Adiciona requisito 18

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function getEmailOrPhone() {
 
 const btn = document.getElementById('button-login');
 btn.addEventListener('click', getEmailOrPhone);
+
+function requiredCamp() {
+  const inputs = document.querySelectorAll('.right-content input');
+  console.log('estou funcionando')
+  for (let index = 0; index < inputs.length; index += 1) {
+    if (inputs[index].value === '') {
+      alert('Campos inválidos');
+      break;
+    }
+  }
+}
+
+let buttonVerifier = document.getElementById('facebook-register');
+buttonVerifier.addEventListener('click', requiredCamp);
