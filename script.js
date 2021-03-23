165d367ef7df09f13474f0b120d81c104721a19a
Alan Albuquerque <alan.alb.flopes@gmail.com>
2021-03-22 21:31:01 -0400

    add req[0,10].rerun[4]

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const buttonLogin = document.getElementById('button-login');
-const inputEmailTel = document.getElementById('user-email-phone');
 buttonLogin.addEventListener('click', (Event) => {
   Event.preventDefault();
-  if (inputEmailTel.value === '') {
-    alert('Email ou telefone');
-  }
+  alert('Email ou telefone');
 });
