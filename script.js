f79b7264763ca8aa7e3cfce148b7228c48bb608b
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-23 09:08:19 -0300

    Teste da troca de mensagem do form invalido

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ function alerta() {
 botaoEntrar.addEventListener('click', () => {
   alerta();
 });
+
+const botaoCadastre = document.getElementById('facebook-register');
+
+botaoCadastre.addEventListener('click', () => {
+  const formField = document.getElementsByTagName('input');
+  for (let index = 0; index < formField.length; index += 1) {
+    formField[index].oninvalid = (e) => {
+      e.target.setCustomValidity('');
+      if (!e.target.validity.valid) {
+        e.target.setCustomValidity('Campos inválidos');
+      }
+    };
+    formField[index].oninput = (e) => {
+      e.target.setCustomValidity('');
+    };
+  }
+});
