e0ccad5cfb2804f9b26ddd7195aaa11b97802838
Hernanne <hernannedomiciano@gmail.com>
2021-03-23 10:45:16 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button = document.getElementById('button-login'); // Variavel recebendo o botão
 const emailPhone = document.getElementById('user-email-phone');
 
-button.addEventListener('click', clickButton);
-function clickButton () {
+function clickButton() {
   window.alert(emailPhone.value);
-};
+}
+button.addEventListener('click', clickButton);
