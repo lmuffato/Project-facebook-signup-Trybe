1d57ffbc10b368222f50624216e833deabda2b5b
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-03-23 10:21:58 -0300

    requisito 18 e inicio do 19

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ function getContact() {
 }
 
 btn.addEventListener('click', getContact);
+
+// requisito 18 validar se todos os campos foram preenchidos
+const submit = document.querySelector('#facebook-register');
+const inputForm = document.querySelectorAll('.input-form');
+
+function validateInputs() {
+  for (let index = 0; index < inputForm.length; index += 1) {
+    if (inputForm[index].value === '') {
+      alert('Campos inválidos');
+    }
+  }
+}
+
+submit.addEventListener('click', validateInputs);
+
+// requisito 19 adicionar campo personalizado
+const personalizado = document.querySelector('#personalizado');
+
+function addInput() {
+  const input = document.createElement('input');
+  personalizado.appendChild(input);
+  input.name = 'gender-custom';
+  input.placeholder = 'Gênero (opcional)';
+}
+
+personalizado.addEventListener('click', addInput);
