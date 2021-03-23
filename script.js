7f7d9e307c1c7701d1bf18456989fc782d02481f
maricegalla <maricegalla@gmail.com>
2021-03-23 00:16:40 -0300

    requisito 19

--- script.js
+++ script.js
@@ -1,37 +1,26 @@
-document.getElementById('label-gender1').onclick = function() {
-  
-  const genderPersonalized = document.getElementsByName("gender");
-  let createInput = document.getElementById('hidden-input');
-  
-  for (let index = 0; index < genderPersonalized.length; index += 1) {
-      if (genderPersonalized[index].value === 'Feminino') {
-        createInput.style.display = 'none';
-      } 
-      
-  }
-};
-document.getElementById('label-gender2').onclick = function() {
-  
-  const genderPersonalized = document.getElementsByName("gender");
-  let createInput = document.getElementById('hidden-input');
-  
-  for (let index = 0; index < genderPersonalized.length; index += 1) {
-      if (genderPersonalized[index].value === 'Masculino') {
-        createInput.style.display = 'none';
-      } 
-      
-  }
-};
-document.getElementById('label-gender3').onclick = function() {
-  
-  const genderPersonalized = document.getElementsByName("gender");
-  let createInput = document.getElementById('hidden-input');
-  
-  for (let index = 0; index < genderPersonalized.length; index += 1) {
-      if (genderPersonalized[index].value === 'Personalizado') {
-        createInput.style.display = 'flex';
-      } 
-      
-  }
-};
+const createInput = document.getElementById('hidden-input');
 
+document.getElementById('label-gender1').onclick = function genderFemale() {
+  const personalizedGender = document.getElementsByName('gender');
+  for (let index = 0; index < personalizedGender.length; index += 1) {
+    if (personalizedGender[index].value === 'Feminino') {
+      createInput.style.display = 'none';
+    }
+  }
+};
+document.getElementById('label-gender2').onclick = function genderMale() {
+  const personalizedGender = document.getElementsByName('gender');
+  for (let index = 0; index < personalizedGender.length; index += 1) {
+    if (personalizedGender[index].value === 'Masculino') {
+      createInput.style.display = 'none';
+    }
+  }
+};
+document.getElementById('label-gender3').onclick = function genderPers() {
+  const personalizedGender = document.getElementsByName('gender');
+  for (let index = 0; index < personalizedGender.length; index += 1) {
+    if (personalizedGender[index].value === 'Personalizado') {
+      createInput.style.display = 'flex';
+    }
+  }
+};
