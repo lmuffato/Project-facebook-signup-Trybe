6c39d39abb7521200e91fd8068b6ffa9d608cb24
renancosta21 <renancosta21@gmail.com>
2021-03-23 00:52:16 -0300

    requisito 18

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function clickLoginBtn() {
 
 const loginBtn = document.getElementById('button-login');
 loginBtn.addEventListener('click', clickLoginBtn);
+
+function check(evt) {
+  evt.preventDefault();
+  const inputs = document.querySelectorAll('.form-input');
+  const signupForm = document.querySelector('.signup-form');
+  for (let index = 0; index <= inputs.length; index += 1) {
+    if (inputs[index].value === '') {
+      const invalidInput = document.createElement('span');
+      invalidInput.innerText = 'Campos Inválidos';
+      signupForm.appendChild(invalidInput);
+      return;
+    }
+  }
+}
+
+const signUpBtn = document.getElementById('facebook-register');
+signUpBtn.addEventListener('click', check);
