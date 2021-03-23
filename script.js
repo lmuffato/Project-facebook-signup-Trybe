5b3ed890a25a142c9d6eb763ca055e14c8a3c574
Maria Lisboa <marialisboa1515@gmail.com>
2021-03-23 17:41:15 -0300

    corrigindo erro linst

--- script.js
+++ script.js
@@ -4,23 +4,3 @@ const emailFone = document.querySelector('#user-email-phone');
 buttonLogin.addEventListener('click', () => {
   window.alert(emailFone.value);
 });
-
-// const personalizado = document.getElementById("personalizar");
-// function criarInput (chek){
-//   let divConteiner = document.getElementById("radio-conteiner");
-// if(chek.target.checked === true ){
-//     let input = document.createElement("input"); 
-//     input.setAttribute("name", "gender-custom");
-//     input.setAttribute("placeholder", "Gênero (opcional)");
-//     input.classList.add("inputDinamico");
-//     divConteiner.appendChild(input);
-
-//   }
-//   else {
-//     document.getElementsByClassName("inputDinamico").remove();
-
-//   }
-// }
-// var btAdicionar = document.getElementById("personalizar");
-// btAdicionar.addEventListener("click", function (e){
-// criarInput(e)});
