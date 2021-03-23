b45501370b98c9ec3c661bb920fdb780a075e1e0
Flavio Franco <flavio.win@gmail.com>
2021-03-23 09:07:43 -0300

    alterando funcao do requisito 18

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const btnSubmit = document.getElementById('facebook-register');
 const registerForm = document.getElementById('register-form');
 
-btnSubmit.addEventListener('click', () => {
+function invalidFields(event) {
+  event.preventDefault();
   const inputContent = document.getElementsByTagName('input');
+  const labelInvalid = document.querySelector('.labelInvalid');
+
+  if (labelInvalid) {
+    return 0;
+  }
 
   for (let index = 0; index < 5; index += 1) {
     if (inputContent[index].value === '') {
       const alertEmpty = document.createElement('span');
       alertEmpty.innerHTML = 'Campos inválidos';
+      alertEmpty.className = 'labelInvalid';
       registerForm.appendChild(alertEmpty);
       break;
     }
   }
-});
+}
+
+btnSubmit.addEventListener('click', invalidFields);
