386dc00ca5eda655688d05b174904b4fc97df04b
Flavio Franco <flavio.win@gmail.com>
2021-03-23 12:08:35 -0300

    requisito 20

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const registerForm = document.getElementById('register-form');
 
 function invalidFields(event) {
   event.preventDefault();
-  const inputContent = document.getElementsByTagName('input');
+  const inputContent = document.getElementsByClassName('registerField');
   const labelInvalid = document.querySelector('.labelInvalid');
 
   if (labelInvalid) {
@@ -49,7 +49,57 @@ function binary() {
   binaryContainer.appendChild(binaryInput);
 }
 
+function verifyChecked() {
+  for (let index = 0; index <= selectBinary.length; index += 1) {
+    if (selectBinary[index].checked) {
+      return selectBinary[index].value;
+    }
+  }
+}
+
+function gatherData() {
+  const joinArray = [];
+
+  const name = document.getElementsByName('firstname')[0].value;
+  const lastName = document.getElementsByName('lastname')[0].value;
+
+  joinArray.push(`Olá, ${name} ${lastName}`);
+  joinArray.push(document.getElementsByName('phone_email')[0].value);
+  joinArray.push(document.getElementsByName('birthdate')[0].value);
+  joinArray.push(verifyChecked());
+
+  return joinArray;
+}
+
+function fillForm(event) {
+  const rightContent = document.querySelector('.right-content');
+
+  for (let index = 0; index < event.length; index += 1) {
+    const myDiv = document.createElement('div');
+    myDiv.innerHTML = event[index];
+    rightContent.appendChild(myDiv);
+  }
+}
+
+function submitForm() {
+  const labelInvalid = document.querySelector('.labelInvalid');
+  const rightContent = document.querySelector('.right-content');
+
+  if (labelInvalid) {
+    return 0;
+  }
+
+  const myData = gatherData();
+
+  while (rightContent.firstChild) {
+    rightContent.removeChild(rightContent.lastChild);
+  }
+
+  fillForm(myData);
+}
+
 selectBinary[0].addEventListener('change', removeBinary);
 selectBinary[1].addEventListener('change', removeBinary);
 selectBinary[2].addEventListener('change', binary);
 btnSubmit.addEventListener('click', invalidFields);
+btnSubmit.addEventListener('click', submitForm);
