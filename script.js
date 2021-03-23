4c53916e240c0f1c8f2c778a24f32ffd579fa0e0
Rodrigo Grande <rogrande0496@gmail.com>
2021-03-23 19:39:20 -0300

    fix reuisitos

--- script.js
+++ script.js
@@ -37,5 +37,18 @@ function checkData(e) {
     document.getElementById('mensage-invalid').style.display = 'none';
   }
 }
+
+function addGenderOption() {
+  for (let j = 0; j < radiosButtons.length; j += 1) {
+    radiosButtons[j].addEventListener('click', addGenderOption);
+    if (radiosButtons[2].checked === true) {
+      document.getElementById('gender-custom').style.display = 'flex';
+    } else {
+      document.getElementById('gender-custom').style.display = 'none';
+    }
+  }
+}
+
 buttonSubmit.addEventListener('click', submit);
 buttonRegister.addEventListener('click', checkData);
+radiosButtons[2].addEventListener('click', addGenderOption);
