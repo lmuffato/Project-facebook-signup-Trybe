fb4816cc92bff18682e6ceb23373aed843d400c7
AdelinoJnr <adelinojunior96@gmail.com>
2021-03-23 12:07:40 -0300

    Validaçao e apariçao do conteudo 'Campos invalidos'

--- script.js
+++ script.js
@@ -4,50 +4,67 @@ button.addEventListener('click', () => {
   alert(valorDoInput.value);
 });
 
+const valorP = document.querySelector('.text-invalido');
+const textInvalido = 'Campos inválidos';
+
 function verificaNome() {
   const valorNome = document.getElementById('firstname');
-  if (valorNome.value === '') {
-    return true;
+  if (valorP.value === undefined) {
+    if (valorNome.value === '') {
+      valorP.innerHTML = textInvalido;
+    }
   }
 }
 
 function verificaSobrenome() {
   const valorSobrenome = document.getElementById('lastname');
-  if (valorSobrenome.value === '') {
-    return true;
+  if (valorP.value === undefined) {
+    if (valorSobrenome.value === '') {
+      valorP.innerHTML = textInvalido;
+    }
   }
 }
 
 function verificaEmailPhone() {
   const valorEmailPhone = document.getElementById('phone_email');
-  if (valorEmailPhone.value === '') {
-    return true;
+  if (valorP.value === undefined) {
+    if (valorEmailPhone.value === '') {
+      valorP.innerHTML = textInvalido;
+    }
   }
 }
 
 function verificaSenha() {
   const valorSenha = document.getElementById('password');
-  if (valorSenha.value === '') {
-    return true;
+  if (valorP.value === undefined) {
+    if (valorSenha.value === '') {
+      valorP.innerHTML = textInvalido;
+    }
   }
 }
 
 function verificaData() {
   const valorData = document.getElementById('datanasc');
-  if (valorData.value === '') {
-    return true;
+  if (valorP.value === undefined) {
+    if (valorData.value === '') {
+      valorP.innerHTML = textInvalido;
+    }
   }
 }
 
-
 function verificaInputsVazios() {
-  if (verificaNome() || verificaSobrenome() || verificaEmailPhone() || verificaSenha() || verificaData()) {
-    console.log('entrou no if do verificaInputsVazios');
-    const paragrafo = document.createElement('p');
-    const divPai = document.querySelector('.msg-invalido');
-    paragrafo.innerHTML = 'Campos inválidos';
-    divPai.appendChild(paragrafo);
-  }
+  verificaNome();
+  verificaSobrenome();
+  verificaEmailPhone();
+  verificaSenha();
+  verificaData();
+  // if (verificaNome() || verificaSobrenome() || verificaEmailPhone() || verificaSenha() || verificaData()) {
+  //   console.log('entrou no if do verificaInputsVazios');
+  //   const paragrafo = document.createElement('p');
+  //   const divPai = document.querySelector('.msg-invalido');
+  //   paragrafo.innerHTML = 'Campos inválidos';
+  //   divPai.appendChild(paragrafo);
+  // }
 }
 
 const botaoCadastro = document.getElementById('facebook-register');
