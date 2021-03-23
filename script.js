6e00a71ed5fee8dfa76291249e4be89016b92092
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-22 21:57:25 -0300

    Correction Requirements to 17(2)

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const selectButton = document.querySelector('#button-login');
 const selectInput = document.querySelector('#user-email-phone');
-// const sendForm = document.querySelector('#facebook-register');
-// const selectInputs = document.querySelectorAll('.inputs, .inputs2, .inputRadio');
+const sendForm = document.querySelector('#facebook-register');
+const selectInputs = document.querySelectorAll('.inputs, .inputs2, .inputRadio');
 // const formSignUp = document.querySelector('#form-signup');
 // const rightContent = document.querySelector('.right-content');
 // const signupForm = document.querySelector('#signup-form');
-// const divMensagem = document.querySelector('#mensagem');
+const divMensagem = document.querySelector('#mensagem');
 
 function alertInput() {
   alert(selectInput.value);
 }
+
+function checkInputs() {
+  let msg = '';
+  for (let index = 0; index < selectInputs.length; index += 1) {
+    const element = selectInputs[index];
+    if (element.value === '') {
+      msg += 'Campos inválidos';
+    } if (element.type === 'radio' && !element.checked) {
+      msg += 'Campos inválidos';
+    }
+  }
+  divMensagem.innerHTML = msg;
+}
+
 // function checkInputs() {
 //   let mensagem = '';
 //   for (let index = 0; index < selectInputs.length; index += 1) {
@@ -24,4 +38,19 @@ function alertInput() {
 // }
 
 selectButton.addEventListener('click', alertInput);
-// sendForm.addEventListener('click', checkInputs);
+sendForm.addEventListener('click', checkInputs);
+
+function preventDefaultSettings(event) {
+  event.preventDefault();
+  const allInputs = document.querySelectorAll('input', 'select', 'textarea');
+  let msg = '';
+  for (let index = 0; index < allInputs.length; index += 1) {
+    const element = allInputs[index];
+    if (element.value === '') {
+      msg += `Campo ${element.id} está vazio `;
+    } if (element.type === 'radio' && !element.checked) {
+      msg += `Campo ${element.id} não esta selecionado `;
+    }
+  }
+  alert(msg);
+}
