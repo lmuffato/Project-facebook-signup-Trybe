f63942846df54af9a0cc92e343426b8359f67392
Flavio Franco <flavio.win@gmail.com>
2021-03-22 23:00:54 -0300

    requisito 18

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const btnSubmit = document.getElementById('facebook-register');
+const registerForm = document.getElementById('register-form');
 
 btnSubmit.addEventListener('click', () => {
   const inputContent = document.getElementsByTagName('input');
 
   for (let index = 0; index < 5; index += 1) {
     if (inputContent[index].value === '') {
-      inputContent[index].value = 'Campos inválidos';
+      const alertEmpty = document.createElement('span');
+      alertEmpty.innerHTML = 'Campos inválidos';
+      registerForm.appendChild(alertEmpty);
+      break;
     }
   }
 });
