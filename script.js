54579dbd9bddda8549bb85c4d6fdb894dc21052a
Iago de Paula <iago.pferreiravr@gmail.com>
2021-03-23 01:22:57 -0300

    Requisito 20 correcao

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const registerBtn = document.querySelector('#facebook-register');
 const invalidField = document.querySelector('#invalid-field');
 let validation = 0;
 const mainContent = document.querySelector('.main-content');
+let selected = '';
 
 console.log(`Aqui estão os inputs: ${inputs.length}`);
 
@@ -19,7 +20,7 @@ loginBtn.addEventListener('click', (e) => {
 
 // Mudar para focus para exibir e blur para remover
 genderField.addEventListener('click', (e) => {
-  const selected = e.target;
+  selected = e.target;
   if (selected === personal) {
     const opcional = document.createElement('input');
     opcional.setAttribute('type', 'text');
@@ -27,6 +28,7 @@ genderField.addEventListener('click', (e) => {
     opcional.setAttribute('placeholder', 'Gênero (opcional)');
     genderField.appendChild(opcional);
   }
+  return selected;
 });
 
 // genderField.addEventListener('click', (e) => {
@@ -39,15 +41,18 @@ genderField.addEventListener('click', (e) => {
 
 function createMessage() {
   const name = document.querySelector('#name');
-  console.log(`Aqui está o nome ${name.value}`);
   const lastname = document.querySelector('#lastname');
   const emailOrPhone = document.querySelector('#phone_email');
   const birthdate = document.querySelector('#birthdate');
+  const genderMessage = selected;
+  console.log(`Aqui está o gênero ${genderMessage}`);
   const message = document.createElement('h4');
   message.innerText = `Olá, ${name.value} 
-  ${lastname.value}, ${emailOrPhone.value}, ${birthdate.value}`;
+  ${lastname.value}, 
+  ${emailOrPhone.value}, ${birthdate.value}, ${genderMessage.value}`;
   const helloStrange = document.createElement('section');
-  helloStrange.id = ('hello-strange');
+  helloStrange.id = 'hello-strange';
+  helloStrange.className = 'right-content';
   helloStrange.appendChild(message);
   mainContent.appendChild(helloStrange);
 }
@@ -58,14 +63,16 @@ registerBtn.addEventListener('click', (e) => {
     if (index >= 2 && inputs[index].value === '') {
       invalidField.innerText = 'Campos inválidos';
       validation = 1;
+      e.preventDefault();
       break;
     }
+    validation = 0;
   }
   console.log(validation);
   if (validation === 0) {
     invalidField.innerText = '';
     createMessage();
     mainContent.removeChild(origRightContent);
+    e.preventDefault();
   }
-  e.preventDefault();
 });
