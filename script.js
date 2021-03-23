08c8e3464ae881c4ec5cf64714cb81284814d36b
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-22 23:31:17 -0300

    Correction to requirement 18 Done

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const selectButton = document.querySelector('#button-login');
 const selectInput = document.querySelector('#user-email-phone');
 const sendForm = document.querySelector('#facebook-register');
 const slctInput = document.querySelectorAll('.main-content input');
+const selectForm = document.querySelector('#form-signup');
+const createSpan = document.createElement('span');
 
 function alertInput() {
   alert(selectInput.value);
@@ -11,7 +13,8 @@ function checkInputs() {
   for (let index = 0; index < slctInput.length; index += 1) {
     const element = slctInput[index];
     if (element.value === '') {
-      alert('Campos inválidos');
+      createSpan.innerText = 'Campos inválidos';
+      selectForm.appendChild(createSpan);
     }
   }
 }
