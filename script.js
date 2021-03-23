e49ef2683568ae3334673c2b2c482e90365ec9a0
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-03-23 11:08:05 -0300

    Requisito 18 - valida preenchimento dos campos do form

--- script.js
+++ script.js
@@ -6,6 +6,32 @@ function activeAlert() {
   });
 }
 
+function checkInputs () {
+  const inputs = document.querySelectorAll('.main-form input');
+  let emptyField = false;
+  for (let index = 0; index < inputs.length-3; index += 1) {
+    if (inputs[index].value === '') {
+      console.log(inputs[index]);
+      emptyField = true;
+    }
+  }
+
+  const inputsRadio = document.getElementsByName('gender');
+  let count = 0;
+  for (let indexRadios = 0; indexRadios < inputsRadio.length; indexRadios += 1) {
+    if (inputsRadio[indexRadios].checked) {
+      count += 1;
+    }
+  }
+
+  if (emptyField === true || count === 0) {
+    alert('Campos inválidos');
+  }
+}
+
+const submitButton = document.querySelector('#facebook-register');
+submitButton.addEventListener('click', checkInputs);
+
 window.onload = function startSession() {
   activeAlert();
 };
