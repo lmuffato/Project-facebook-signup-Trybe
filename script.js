dd20972b417aeae1dcfc85122dbfb341ea277d40
Alexandre Oliveira <alexandremolive@gmail.com>
2021-03-23 15:59:35 -0300

    ajuste no layout das funcoes

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 const btnLogin = document.querySelector('#button-login');
 const userEmail = document.querySelector('#user-email-phone');
 
-function alertEmail() {
+btnLogin.addEventListener('click', () => {
   alert(userEmail.value);
-}
-
-btnLogin.addEventListener('click', alertEmail);
+});
 
 const btnRegister = document.querySelector('#facebook-register');
-function checkInputs(event) {
+btnRegister.addEventListener('click', (event) => {
   const userInputs = document.querySelectorAll('.user-data');
   event.preventDefault();
-
   for (let index = 0; index < userInputs.length; index += 1) {
     if (userInputs[index].value === '') {
       const formAlert = document.querySelector('.create-acount');
@@ -21,5 +18,4 @@ function checkInputs(event) {
       break;
     }
   }
-}
-btnRegister.addEventListener('click', checkInputs);
+});
