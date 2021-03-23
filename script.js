e396040e983de0445d91d2e2aa8504ac843aeee0
Rodrigo Rech <rodrigorech92@gmail.com>
2021-03-23 10:29:04 -0300

    Requerimento 19 - Botao genero personalizado abre textBox

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const emailLoginBtn = document.getElementById('button-login');
 
+const genderPersonalizado = document.getElementById('gender-personalizado');
+
+const boxPersonalizado = document.getElementById('boxPersonalizado');
+
+const mascBtn = document.getElementById('mascBtn');
+
+const femBtn = document.getElementById('femBtn');
+
 function emailAlert() {
   const emailLoginValue = document.getElementById('user-email-phone').value;
   alert(emailLoginValue);
 }
 
 emailLoginBtn.addEventListener('click', emailAlert);
+
+function openTextBox() {
+  boxPersonalizado.removeAttribute('class');
+}
+
+genderPersonalizado.addEventListener('click', openTextBox);
+
+function closeTextBox() {
+  boxPersonalizado.className = 'personalizadoBox';
+}
+
+femBtn.addEventListener('click', closeTextBox);
+
+mascBtn.addEventListener('click', closeTextBox);
