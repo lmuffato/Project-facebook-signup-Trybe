27ae89441812c0979dc4e872617a3ab3372913ba
Ana Ventura <anaventura1811@gmail.com>
2021-03-23 02:24:43 -0300

    Tentativa requisito 18

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ btnLogin.addEventListener('click', () => {
   const emailOuTelefone = document.getElementById('user-email-phone');
   alert(emailOuTelefone.value);
 });
+
+//  Exibir uma mensagem "Campos inválidos" dentro do formulário caso pelo menos um campo não esteja preenchido
+const facebookForm = document.getElementById('facebook-form');
+const span = document.createElement('span');
+function validatingForm(event) {
+  for (let key = 0; key < facebookForm.elements.length; key += 1) {
+    if (facebookForm.elements[key].value === '') {
+      event.preventDefault();
+      span.innerHTML = 'Campos inválidos';
+      facebookForm.appendChild(span);
+      console.log('estou funfando');
+    }
+  }
+}
+
+const btnSubmit = document.getElementById('facebook-register');
+btnSubmit.addEventListener('click', validatingForm);
