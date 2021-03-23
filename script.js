7b4e66ce6c4b4259d321760b3a7ed32a9590e4cb
Iago de Paula <iago.pferreiravr@gmail.com>
2021-03-23 00:06:24 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const loginBtn = document.querySelector('#button-login');
 const userEmailPhone = document.querySelector('#user-email-phone');
 const personal = document.querySelector('#personal');
 const genderField = document.querySelector('#gender-field');
+const inputs = document.getElementsByTagName('input');
+let validation = 0;
+const registerBtn = document.querySelector('#facebook-register');
+const invalidField = document.querySelector('#invalid-field');
 
-console.log(`Personal: ${personal.innerHTML}`);
-console.log(`Gender Field: ${genderField.innerHTML}`);
+console.log(`Aqui estão os inputs: ${inputs.length}`);
+
 loginBtn.addEventListener('click', (e) => {
   if (userEmailPhone.innerText === '') {
     alert(userEmailPhone.value);
@@ -21,5 +25,18 @@ genderField.addEventListener('click', (e) => {
     opcional.setAttribute('placeholder', 'Gênero (opcional)');
     genderField.appendChild(opcional);
   }
-  console.log(`Selected: ${selected.innerHTML}`);
+});
+
+registerBtn.addEventListener('click', (e) => {
+  for (let index = 0; index < inputs.length; index += 1) {
+    if (index >= 2 && inputs[index].value === '') {
+      invalidField.innerText = 'Campos inválidos';
+      validation = 1;
+      break;
+    } else {
+      invalidField.innerText = '';
+    }
+  }
+  e.preventDefault();
+  return validation;
 });
