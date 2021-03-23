edcece32e69e5c61ede9da57a86da4b41033e45c
Luiz Felipe Araujo <thelastnailmusic@gmail.com>
2021-03-23 11:09:43 -0300

    Desafio 17 pela metade

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ const valorDoInput = document.querySelector('#user-email-phone');
 button.addEventListener('click', () => {
   alert(valorDoInput.value);
 });
+
+function verificaNome() {
+  const valorNome = document.getElementById('firstname');
+  console.log('Entrou no verificaNome');
+  if (valorNome.value === '') {
+    alert('Nome Vazio');
+  } else {
+    alert('Entrou no ELSE');
+  }
+}
+
+function verificaInputsVazios() {
+  verificaNome();
+}
+
+const botaoCadastro = document.getElementById('facebook-register');
+botaoCadastro.addEventListener('click', verificaInputsVazios);
