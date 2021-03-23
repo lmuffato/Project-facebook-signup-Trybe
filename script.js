1f9c0011cab58ee4a98df986ed6049aafffd5948
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-03-23 03:49:20 -0300

    Alterações no formulário

--- script.js
+++ script.js
@@ -6,27 +6,27 @@ window.onload = () => {
   });
 };
 
-// function genderCustom() {
-//   if (document.querySelector('#customGender').checked === true) {
-//     const parentEle = document.querySelector('#personalizedGender');
-//     const elem = document.createElement('input');
-//     parentEle.appendChild(elem);
-//     Object.assign(elem, {
-//       name: genderCustomConst,
-//       type: 'text',
-//       placeholder: 'Gênero (opcional)',
-//       id: genderCustomConst,
-//     });
-//   }
-// }
+function genderCustom() {
+  if (document.querySelector('#customGender').checked === true) {
+    const parentEle = document.querySelector('#personalizedGender');
+    const elem = document.createElement('input');
+    parentEle.appendChild(elem);
+    Object.assign(elem, {
+      name: genderCustomConst,
+      type: 'text',
+      placeholder: 'Gênero (opcional)',
+      id: genderCustomConst,
+    });
+  }
+}
 
-// function Clicou1(evento) {
-//   console.log('mouse clicou no botão 1');
-//   console.log(evento.target);
-//   genderCustom();
-// }
+function Clicou1(evento) {
+  console.log('mouse clicou no botão 1');
+  console.log(evento.target);
+  genderCustom();
+}
 
-// document.getElementById('customGender').addEventListener('click', Clicou1);
+document.getElementById('customGender').addEventListener('click', Clicou1);
 
 function clickParaSelecionar() {
   document.getElementById(genderCustomConst).remove();
