0031c2a007c9079e3b75decd7a0530d9b5904a39
Matheus Lima Pimenta <mateus.krauss@gmail.com>
2021-03-22 22:53:12 -0300

    Adiciona botão ao lado de input

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ window.onload = function startin() {
   const btnLogin = document.getElementById('button-login');
   btnLogin.addEventListener('click', () => {
     const inputName = document.getElementById('user-email-phone-label').value;
-    const inputPw = document.getElementById('user-password-label').value;
-    alert(`Login: ${inputName}  Senha:  ${inputPw}`);
+    alert(`Login: ${inputName}`);
   });
 };
