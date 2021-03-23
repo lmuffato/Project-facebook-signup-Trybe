1fef6e9f746d17ac38f9ec43358bb9cd2c4530f1
GetulioGutemberg <getulio.dev@gmail.com>
2021-03-23 19:47:18 -0300

    ajustas codigos

--- script.js
+++ script.js
@@ -4,39 +4,3 @@ buttonLogin.addEventListener('click', (Event) => {
   Event.preventDefault();
   alert(inputEmailTel.value);
 });
-
-
-const buttonRegister = document.getElementById('facebook-register');
-buttonRegister.addEventListener('click', (Event) => {
-  Event.preventDefault();
-  if(document.getElementsByName('firstname').value === null) {
-    // const elementVadidator = document.getElementsByName('firstname');
-    // elementVadidator.appendChild('Campos inválidos'); 
-    alert('Campos inválidos');
-  }
-  if(document.getElementsByName('lastname').value === null) {
-    // const elementVadidator = document.getElementsByName('lastname');
-    // elementVadidator.appendChild('Campos inválidos');
-    alert('Campos inválidos');
-  }
-  if(document.getElementsByName('phone_email').value === null) {
-    // const elementVadidator = document.getElementsByName('phone_email');
-    // elementVadidator.appendChild('Campos inválidos');
-    alert('Campos inválidos');
-  }
-  if(document.getElementsByName('password').value === null) {
-    // const elementVadidator = document.getElementsByName('password');
-    // elementVadidator.appendChild('Campos inválidos');
-    alert('Campos inválidos');
-  }
-  if(document.getElementsByName('gender').value === null) {
-    // const elementVadidator = document.getElementsByName('gender');
-    // elementVadidator.appendChild('Campos inválidos');
-    alert('Campos inválidos');
-  }
-  if(document.getElementsByName('birthdate').value === null) {
-    // const elementVadidator = document.getElementsByName('birthdate');
-    // elementVadidator.appendChild('Campos inválidos');
-    alert('Campos inválidos');
-  }
-});
