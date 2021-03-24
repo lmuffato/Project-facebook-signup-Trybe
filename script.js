9ba6a33f819adada566ff516f5481749d45db476
lucas-chave <lcchavesr150@gmail.com>
2021-03-23 21:43:46 -0300

    adicionando requisito 12

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 const button = document.getElementById('button-login');
 const textInput = document.getElementById('user-email-phone');
+const radioButton = document.getElementById('custom');
+const label = document.getElementById('label-gender');
+const man = document.getElementById('man');
+const woman = document.getElementById('woman');
+
+function addInput() {
+	if(radioButton.value) {
+		const genderText = document.createElement('input');
+		genderText.setAttribute('type', 'text');
+		label.appendChild(genderText);
+		genderText.classList.add('customGender');
+	}  
+
+}
+
+radioButton.addEventListener('click', addInput);
+
+// function removeInput() {
+// 	const removeGender = document.getElementsByClassName('customGender');
+//   if(man.value) {
+// 		label.removeChild('removeGender'); 
+// 	}  
+// }
+
+// man.addEventListener('click', removeInput);
+// woman.addEventListener('click', removeInput);
+
+
 
 function clickButton() {
   const input = textInput.value;
