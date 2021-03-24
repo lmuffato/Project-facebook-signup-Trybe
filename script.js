8bf6c2d21a73fd2290f8b620067b692e9ec3d1ea
Maria Lisboa <marialisboa1515@gmail.com>
2021-03-24 10:34:56 -0300

    corrigimos erro do avaliador

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ const emailFone = document.querySelector('#user-email-phone');
 buttonLogin.addEventListener('click', () => {
   window.alert(emailFone.value);
 });
+
+const personalizado = document.getElementById("personalizar");
+function criarInput (chek){
+let divConteiner = document.getElementById("radio-conteiner");
+
+if(chek.target.checked === true ){
+let input = document.createElement("input"); 
+input.setAttribute("name", "gender-custom");
+input.setAttribute("placeholder", "Gênero (opcional)");
+input.classList.add("inputDinamico");
+divConteiner.appendChild(input);
+console.log(input);
+}
+else if(input.classList==="inputDinamico") {
+input.remove("inputDinamico");
+
+}
+}
+var btAdicionar = document.getElementById("personalizar");
+btAdicionar.addEventListener("click", function (e){
+criarInput(e)});
