bf44725f1b9b5e9d9ee378caeb1ea4618b402e33
Victor Ricco <victor.ricco001@gmail.com>
2021-03-24 15:03:11 -0300

    cypress errors

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ function alertButton() {
 }
 const botao = document.getElementById('button-login');
 botao.addEventListener('click', alertButton);
+
+const personalized = document.querySelector('#personalized');
+
+personalized.addEventListener('click', () => {
+  const genderOptions = document.querySelector('#genders');
+  const childNumber = genderOptions.childNodes;
+  if(childNumber.length === 0){
+    const personalizedGender = document.createElement('input');
+    personalizedGender.setAttribute('name', 'gender-custom');
+    personalizedGender.setAttribute('placeholder', 'Gênero (opcional)');
+    genderOptions.appendChild(personalizedGender);
+  }
+})
+
+const removePersonalGender = () => {
+  const noGenderDiv = document.getElementById('genders');
+  const childrenDiv = noGenderDiv.childNodes;
+  if (childrenDiv.length > 0) {
+    noGenderDiv.removeChild(noGenderDiv.firstChild);
+  }
+};
