3a2bbe65ce3bf44d4706483d1e188296e56bcb17
Edrei <edreimateus@gmail.com>
2021-03-24 11:45:51 -0300

    Adiciona requisito 18 e 20

--- script.js
+++ script.js
@@ -13,3 +13,50 @@ function genderCustomInput() {
   }
 }
 otherGender.addEventListener('click', genderCustomInput);
+
+function radiosBtn() {
+  const inputRadios = document.querySelectorAll('.radio-genders input');
+  let checked;
+  for (let index = 0; index < inputRadios.length; index += 1) {
+    if (inputRadios[index].checked) {
+      checked = inputRadios[index].value;
+    }
+  }
+  return checked;
+}
+
+function checkInputs() {
+  let checkInputsValue = false;
+  const allInputs = document.querySelectorAll('.required');
+  for (let index = 0; index < allInputs.length; index += 1) {
+    if (allInputs[index].value === '' || radiosBtn() === undefined) {
+      checkInputsValue = true;
+    }
+  }
+  return checkInputsValue;
+}
+
+function welcomeGreetings() {
+  const rightContent = document.querySelector('.right-content');
+  rightContent.innerText = `
+  Olá, ${document.querySelector('#first-name').value}
+  ${document.querySelector('#last-name').value}
+  ${document.querySelector('#phone-email-input').value}
+  ${document.querySelector('#input-birthdate').value}
+  ${radiosBtn()}`;
+}
+
+window.onload = () => {
+  const registerBtn = document.querySelector('#facebook-register');
+  const errorInput = document.querySelector('#errorInput');
+  registerBtn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    errorInput.innerText = '';
+    if (checkInputs()) {
+      errorInput.innerText = 'Campos inválidos';
+      errorInput.style.color = 'red';
+    } else {
+      welcomeGreetings();
+    }
+  });
+};
