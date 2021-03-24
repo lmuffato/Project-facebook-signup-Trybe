7e9a442f83044ad239181839f5fcfc671ec29e8f
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-24 12:44:58 -0300

    Requisito 18 - verifica se os campos estao preenchidos

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const loginBtn = document.getElementById('button-login');
 const userEmailInput = document.getElementById('user-email-phone');
+const signInBtn = document.getElementById('facebook-register');
+const nameInput = document.getElementById('first-name');
+const lastNameInput = document.getElementById('last-name');
+const cellEmailInput = document.getElementById('phone-email');
+const newPasswordInput = document.getElementById('new-password');
+const birthDateInput = document.getElementById('birth-date');
+const errorMessageDiv = document.getElementById('error-message');
 
 function emitAlert(e) {
   e.preventDefault();
@@ -27,9 +34,42 @@ function HideInput() {
   }
 }
 
+function checkNamesEmail() {
+  if (!nameInput.value || !lastNameInput.value || !cellEmailInput.value) {
+    return false;
+  }
+
+  return true;
+}
+
+function checkPassWordBirth() {
+  if (!newPasswordInput.value || !birthDateInput.value) {
+    return false;
+  }
+
+  return true;
+}
+
+// Consultado em 'How can I check whether a radio button is selected with JavaScript?'
+// https://stackoverflow.com/questions/1423777/how-can-i-check-whether-a-radio-button-is-selected-with-javascript
+function checkRadio() {
+  if (maleInput.checked || femaleInput.checked || otherInput.checked) {
+    return true;
+  }
+
+  return false;
+}
+
+function checkFields() {
+  if (!checkNamesEmail() || !checkPassWordBirth() || !checkRadio()) {
+    errorMessageDiv.classList.remove('hidden');
+  }
+}
+
 otherInput.addEventListener('click', ShowInput);
 maleInput.addEventListener('click', HideInput);
 femaleInput.addEventListener('click', HideInput);
+signInBtn.addEventListener('click', checkFields);
 
 ShowInput();
 HideInput();
