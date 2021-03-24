aa6f1278d85f1d0924e702da8bf9ef7c61397506
hheloisa <hhackenhaar@gmail.com>
2021-03-24 11:08:27 -0300

    Inicio requisito 18

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function alert1() {
 }
 const buttonLogin = document.querySelector('#button-login');
 buttonLogin.addEventListener('click', alert1);
+
+function validateRegister() {
+  const validate = document.querySelectorAll('.validate');
+  if (validate === ' ') {
+    alert("Campos inválidos");
+  }
+}
+
+const buttonRegister = document.querySelector('#facebook-register');
+buttonRegister.addEventListener('click', validateRegister);
