989307221831e346804a569876a9982efe1994b8
Gabriel Ferreira <glucas709@gmail.com>
2021-03-24 14:54:50 -0300

    Requisito 18 - validando campos

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const btnLogin = document.getElementById('button-login');
 const emailPhone = document.getElementById('user-email-phone');
 const otherGenders = document.getElementById('other');
 const formRegister = document.getElementById('form-register');
+const btnRegister = document.getElementById('facebook-register');
 
 btnLogin.addEventListener('click', () => {
   alert(emailPhone.value);
@@ -19,4 +20,23 @@ const genderInput = () => {
   otherGenders.removeEventListener('click', genderInput);
 };
 
+const checkEmpty = () => {
+  const formInputs = formRegister.querySelectorAll('input[type="text"');
+  const genderInput = formRegister.querySelectorAll('input[type="radio"]');
+  const errorMessage = document.getElementById('message-error');
+
+  for (let i = 0; i < formRegister.length; i += 1) {
+    if (formRegister[i] === '' || formRegister[i] !== 'checked') {
+      errorMessage.innerHTML = 'Campos inválidos';
+    }
+  }
+}
+
+formRegister.addEventListener('submit', (e) => {
+  e.preventDefault();
+})
+
 otherGenders.addEventListener('click', genderInput);
+btnRegister.addEventListener('click', () => {
+  checkEmpty();
+})
