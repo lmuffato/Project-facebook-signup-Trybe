2979e99bed5faa55441aa5dbaef4eca23b6b5704
Stanrley Barbosa <3011stan@gmail.com>
2021-03-24 20:39:26 -0300

    Correçao requisito 18 3

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 const getButtonEntrar = document.querySelector('#button-login');
 const fields = document.querySelectorAll('[required]');
-const getButtonRegister = document.querySelector('#facebook-register');
 function spamAlert() {
   alert(document.querySelector('#user-email-phone').value);
 }
 function customValidation() {
   const divInvalid = document.querySelector('#invalid-input');
   const insertText = document.createElement('p');
-  if (document.querySelectorAll('.div-invalida').length > 0) {
-  } else {
+  if (document.querySelectorAll('.div-invalida').length === 0) {
     insertText.innerText = 'Campos inválidos';
     divInvalid.appendChild(insertText);
     insertText.classList.add('div-invalida');
   }
 }
 getButtonEntrar.addEventListener('click', spamAlert);
-for (field of fields) {
-  field.addEventListener("invalid", customValidation);
+
+for (let i = 0; i < fields.length; i += 1) {
+  fields[i].addEventListener('invalid', customValidation);
 }
