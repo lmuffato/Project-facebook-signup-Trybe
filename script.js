49d2c5acdfc8d03bef5e6a3daaa3c8a0ff21dc4a
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-24 00:19:37 -0300

    lint lint lint

--- script.js
+++ script.js
@@ -13,8 +13,10 @@ function createCustomGender() {
   const personalizado = document.querySelector('#Personalizado');
   const genderCustom = document.querySelector('#custom-gender');
   if (personalizado.checked === true) {
-    genderCustom.style.display = "block";
+    genderCustom.style.display = 'block';
   } else {
-    genderCustom.style.display = "none";
-  } 
+    genderCustom.style.display = 'none';
+  }
 }
+
+createCustomGender();
