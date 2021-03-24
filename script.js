f976bf2727ae56fa61928cfff6a29ef5135d0f74
Victor Ricco <victor.ricco001@gmail.com>
2021-03-24 15:07:17 -0300

    Lint

--- script.js
+++ script.js
@@ -11,18 +11,18 @@ const personalized = document.querySelector('#personalized');
 personalized.addEventListener('click', () => {
   const genderOptions = document.querySelector('#genders');
   const childNumber = genderOptions.childNodes;
-  if(childNumber.length === 0){
+  if (childNumber.length === 0) {
     const personalizedGender = document.createElement('input');
     personalizedGender.setAttribute('name', 'gender-custom');
     personalizedGender.setAttribute('placeholder', 'Gênero (opcional)');
     genderOptions.appendChild(personalizedGender);
   }
-})
+});
 
-const removePersonalGender = () => {
-  const noGenderDiv = document.getElementById('genders');
-  const childrenDiv = noGenderDiv.childNodes;
-  if (childrenDiv.length > 0) {
-    noGenderDiv.removeChild(noGenderDiv.firstChild);
-  }
-};
+// const removePersonalGender = () => {
+//   const noGenderDiv = document.getElementById('genders');
+//   const childrenDiv = noGenderDiv.childNodes;
+//   if (childrenDiv.length > 0) {
+//     noGenderDiv.removeChild(noGenderDiv.firstChild);
+//   }
+// };
