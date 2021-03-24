82a6d7f7ad3b7fd427a4f605b52b2a21d325b402
Vinicius Rodrigues <V.rodriguesjk@gmail.com>
2021-03-24 10:34:09 -0300

    iniciando requisito 20

--- script.js
+++ script.js
@@ -55,3 +55,30 @@ function createElement() {
 }
 
 clickCustom.addEventListener('click', createElement);
+
+// Requisito 20//
+
+function getElements() {
+  const name = document.querySelector('input[name="firstname"]').value;
+  const lastName = document.querySelector('input[name="lastname"]').value;
+  const phoneEmail = document.querySelector('input[name="phone_email"]').value;
+  const password = document.querySelector('input[name="password"]').value;
+  const birthdate = document.querySelector('input[name="birthdate"]').value;
+  const result = [name, lastName, phoneEmail, birthdate, password];
+  return result;
+}
+
+function getGender() {
+  let result = '';
+  const fem = document.querySelector('input[value="Feminino"]').checked;
+  const masc = document.querySelector('input[value="Masculino"]').checked;
+  const person = document.querySelector('input[value="Personalizado"]').checked;
+  if (fem === true) {
+    result = 'Feminino';
+  } else if (masc === true) {
+    result = 'Masculino';
+  } else if (person === true) {
+    result = 'Personalizado';
+  }
+  return result;
+}
