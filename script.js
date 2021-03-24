aea912cd4785763d64653a3650b4d8db6da4c9e8
maricegalla <maricegalla@gmail.com>
2021-03-24 12:52:56 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const createInput = document.getElementById('hidden-input');
+const buttonSignUp = document.querySelector('#facebook-register');
+const form = document.getElementById('form');
+const data = document.querySelector('#right-content-elements');
+const rightContent = document.getElementById('print-right');
 
 document.getElementById('label-gender1').onclick = function genderFemale() {
   const personalizedGender = document.getElementsByName('gender');
@@ -24,3 +28,40 @@ document.getElementById('label-gender3').onclick = function genderPers() {
     }
   }
 };
+
+function checkRadio() {
+  const female = document.getElementById('label-gender1');
+  const male = document.getElementById('label-gender2');
+  const personalized = document.getElementById('label-gender3');
+
+  if (female.checked === true) {
+    return female.value;
+  } if (male.checked === true) {
+    return male.value;
+  } return personalized.value;
+}
+
+function dataExibition() {
+  const name = form.elements[0].value;
+  const lastName = form.elements[1].value;
+  const email = form.elements[2].value;
+  const birthDate = form.elements[4].value;
+  const print = `Olá ${name} ${lastName}
+  ${email}
+  ${birthDate}
+  ${checkRadio()}`;
+  return print;
+}
+
+function showPrint() {
+  const divPrint = document.createElement('div');
+  divPrint.innerHTML = dataExibition();
+  divPrint.className = 'print-data';
+  return rightContent.appendChild(divPrint);
+}
+
+buttonSignUp.addEventListener('click', (event) => {
+  event.preventDefault();
+  data.style.display = 'none';
+  showPrint();
+});
