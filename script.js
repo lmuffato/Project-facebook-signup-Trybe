e6f8c399026058377d8f9e813e2d42784f5d0cea
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-03-23 21:43:08 -0300

    requesito 20

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ function buttonAlert() {
 }
 loginButton.addEventListener('click', buttonAlert);
 
+// criando alerta nos required
+
 // add o personalize option gender
 const toBe = document.getElementById('to-be');
 const toBeInput = document.createElement('input');
@@ -27,3 +29,56 @@ const female = document.getElementById('female');
 male.addEventListener('click', removeCamp);
 female.addEventListener('click', removeCamp);
 toBe.addEventListener('click', newCamp);
+
+// requesito 20
+// resgatando o nome do user
+const userNameInForm = () => {
+  const userName = document.getElementById('name-content').children;
+  const realName = `${userName[0].value} ${userName[1].value}`;
+  return realName;
+};
+// resgatando email ou telefone
+const userEmailOrPhone = () => {
+  const userEmail = document.getElementById('phone-mail').children;
+  const outPutEmail = userEmail[0].value;
+  return outPutEmail;
+};
+
+// resgatando data
+const birthDate = () => {
+  const date = document.getElementById('birthdate').children[1].value;
+  return date;
+};
+
+// resgatando gender
+const outPutTheGender = () => {
+  let oqueVaiSair;
+  if (male.checked) {
+    oqueVaiSair = 'Masculino';
+  } else if (female.checked) {
+    oqueVaiSair = 'Feminino';
+  } else {
+    oqueVaiSair = 'Personalizado';
+  }
+  return oqueVaiSair;
+};
+// resgatando o lindo do right-content
+const rightContent = document.querySelector('.right-content');
+// dando inner vazio
+const changeTheRight = () => {
+  rightContent.innerHTML = '';
+};
+// definindo a função com os output
+const outPutContent = () => {
+  const dados = `Olá, ${userNameInForm()}
+  ${userEmailOrPhone()}
+  ${birthDate()}
+  ${outPutTheGender()}`;
+  changeTheRight();
+  rightContent.innerHTML = dados;
+  // rightContent.appendChild[0](elementText('p',dados));
+};
+
+const signUpButton = document.getElementById('facebook-register');
+signUpButton.addEventListener('click', outPutContent);
+// partindo para o abraço #XABLAU o mago é Implacável
