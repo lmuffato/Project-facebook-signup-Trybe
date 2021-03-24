75e2b3f54a2cc2dfb690e0acede115c8051ca4c6
mallmann02 <leomallmann020302@gmail.com>
2021-03-24 01:02:05 -0300

    Requisito 19

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const loginButton = document.querySelector('#button-login');
 const emailPhoneInput = document.querySelector('#user-email-phone');
+const personalizadoRadioButton = document.querySelector('#personalizado');
+const campoDeTextoPersonalizado = document.createElement('input');
+const submitFormButton = document.querySelector('#facebook-register');
+const criaContaForm = document.querySelector('.right-content form');
 
 function exibirAlertaDeLogin() {
   window.alert(emailPhoneInput.value);
 }
 
+function adicionaCampoDeTextoPersonalizado() {
+  campoDeTextoPersonalizado.type = 'text';
+  campoDeTextoPersonalizado.id = 'personalizado-input';
+  campoDeTextoPersonalizado.name = 'gender-custom';
+  campoDeTextoPersonalizado.placeholder = 'Gênero';
+  criaContaForm.insertBefore(campoDeTextoPersonalizado, submitFormButton);
+}
+
 loginButton.addEventListener('click', exibirAlertaDeLogin);
+personalizadoRadioButton.addEventListener('click', adicionaCampoDeTextoPersonalizado);
