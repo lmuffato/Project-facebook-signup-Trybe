3436f51c4058ba87644c7f45db5062a6bd6ddccf
Apoc35 <alexv_silveira@outlook.com>
2021-03-24 00:34:59 -0300

    Resolução do requisito 18, e criação do botão (cadastre-se).

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
+const confirmButton = document.getElementById('facebook-register');
+const inputNames = document.getElementsByClassName('input-fullname');
+const inputCelEmail = document.getElementsByClassName('input-form');
+const inputRadioButtons = document.querySelectorAll('.input-radios input');
+const inputBday = document.getElementById('input-birthdate');
 const alertButton = document.getElementById('button-login');
+
 alertButton.addEventListener('click', () => {
   const loginValue = document.getElementById('user-email-phone').value;
   alert(loginValue);
+});
+
+function verify(elementsArray) {
+  let showAlert = false;
+  for (let index = 0; index < elementsArray.length; index += 1) {
+    if (!elementsArray[index].value) {
+      showAlert = true;
+    }
+  }
+  return showAlert;
+}
+
+function radioVerify(elementsRadio) {
+  let radioValue = true;
+  for (let index = 0; index < inputRadioButtons.length; index += 1){
+    if(elementsRadio[index].checked){
+      radioValue = false;
+    }
+  }
+  return radioValue;
+}
+
+confirmButton.addEventListener('click', () => {
+  if (verify(inputNames) || verify(inputCelEmail) || radioVerify(inputRadioButtons)) {
+    alert('Campos inválidos');
+  }
 });
