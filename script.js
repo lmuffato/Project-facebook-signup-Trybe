4a4d079bb88c8ff84c024c028be782a453a9452e
AdelinoJnr <adelinojunior96@gmail.com>
2021-03-24 11:40:44 -0300

    Correçao do requisito 16 (Corrigindo ordem dos generos)

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ button.addEventListener('click', () => {
   alert(valorDoInput.value);
 });
 
+// const valorSobrenome = document.getElementById('lastname');
+// const valorNome = document.getElementById('firstname');
+// const valorEmailPhone = document.getElementById('phone_email');
+// const valorData = document.getElementById('datanasc');
+// const valorSenha = document.getElementById('password');
 const valorP = document.querySelector('.text-invalido');
 const textInvalido = 'Campos inválidos';
 
@@ -35,36 +40,11 @@ function verificaSenha() {
   }
 }
 
-function verificaData() {
-  const valorData = document.getElementById('datanasc');
-  if (valorP.value === undefined && valorData.value === '') {
-    valorP.innerHTML = textInvalido;
-  }
-}
-
-function verificaInputsVazios() {
-  verificaNome();
-  verificaSobrenome();
-  verificaEmailPhone();
-  verificaSenha();
-  verificaData();
-  // if (verificaNome() || verificaSobrenome() || verificaEmailPhone() || verificaSenha() || verificaData()) {
-  //   console.log('entrou no if do verificaInputsVazios');
-  //   const paragrafo = document.createElement('p');
-  //   const divPai = document.querySelector('.msg-invalido');
-  //   paragrafo.innerHTML = 'Campos inválidos';
-  //   divPai.appendChild(paragrafo);
-  // }
-}
-
-const botaoCadastro = document.getElementById('facebook-register');
-botaoCadastro.addEventListener('click', verificaInputsVazios);
-
-function stopDefAction(event) {
-  event.preventDefault();
-}
-
-botaoCadastro.addEventListener('click', stopDefAction, false);
+// function verificaData() {
+//   if (valorP.value === undefined && valorData.value === '') {
+//     valorP.innerHTML = textInvalido;
+//   }
+// }
 
 const pers = document.getElementById('personalizado');
 const pegaDiv = document.getElementById('genderSpace');
@@ -75,9 +55,46 @@ const criarInput = () => {
     inputGender.type = 'text';
     inputGender.placeholder = 'Gênero (opcional)';
     inputGender.name = 'gender-custom';
+    inputGender.className = 'generos';
     pegaDiv.className = 'existeClasse';
     pegaDiv.appendChild(inputGender);
   }
 };
 
 pers.addEventListener('click', criarInput);
+
+// Requisito 20 (Função que cria a div dos dados no forms)
+// const section = document.querySelector('.right-content');
+
+// const valueNameSobrenome = () => `Olá, ${valorNome.value} ${valorSobrenome.value}`;
+
+// const valueEmailPhone = () => valorEmailPhone.value;
+
+// const valueData = () => valorData.value;
+
+// const valueGenero = () => {
+//   const inputGeneros = document.querySelectorAll('.generos');
+//   for (let index = 0; index < inputGeneros.length: index += 1) {
+//     if (inputGeneros[index].checked) {
+//       return inputGeneros[index].value;
+//     }
+//   }
+// }
+
+function verificaInputsVazios() {
+  verificaNome();
+  verificaSobrenome();
+  verificaEmailPhone();
+  verificaSenha();
+  // verificaData();
+  // createDivDados();
+}
+
+const botaoCadastro = document.getElementById('facebook-register');
+botaoCadastro.addEventListener('click', verificaInputsVazios);
+
+function stopDefAction(event) {
+  event.preventDefault();
+}
+
+botaoCadastro.addEventListener('click', stopDefAction, false);
