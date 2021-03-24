88bcf88e4b535fc93d3c2bd1d370a29f89e10734
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-23 21:18:02 -0300

    Requisito 7

---
+++ script.js
@@ -0,0 +1,6 @@
+// Requisito 7
+const btnEntrar = document.getElementById('button-login');
+const userEmailPhone = document.getElementById('user-email-phone');
+
+btnEntrar.addEventListener('click', () => alert(userEmailPhone.value));
+
