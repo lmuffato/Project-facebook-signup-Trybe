552beb55a3441f6e535206eeacb019da71ac7ed4
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-03-24 10:14:13 -0300

    Requisito 18. Validar se todos os campos foram preenchidos ao clicar no botão Cadastre-se

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const userEmail = document.getElementById('user-email-phone');
 btnEntrar.addEventListener('click', () => {
   alert(userEmail.value);
 });
+
+const btnRegister = document.getElementById('facebook-register');
+const inputSignup = document.querySelectorAll('.input-signup');
+
+btnRegister.addEventListener('click', () => {
+  for (let index = 0; index < inputSignup.length; index += 1) {
+    if (inputSignup[index].value === '') {
+      inputSignup[index].placeholder = "Campos inválidos";
+    }
+  }
+});
