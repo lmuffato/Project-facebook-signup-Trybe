6c381c00ff3dad3459423d6eb0857ea8642a8f5a
Stanrley Barbosa <3011stan@gmail.com>
2021-03-24 12:28:11 -0300

    Pause requisito 18

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const getButtonEntrar = document.querySelector('#button-login');
+const fields = document.querySelectorAll("[required]");
+const getButtonRegister = document.querySelector('#facebook-register');
 
 function spamAlert() {
   alert(document.querySelector('#user-email-phone').value);
 }
 
+// function customValidation() {
+//   const divInvalid = document.querySelector('#invalid-input');
+//   const insertText = document.createElement('p');
+//   insertText.innerText = 'Campos inválidos';
+//   divInvalid.appendChild(insertText);
+//   return;
+// }
+
 getButtonEntrar.addEventListener('click', spamAlert);
+
+// getButtonRegister.addEventListener('click', (event) => {
+//   for(field of fields) {
+//     field.addEventListener("invalid", customValidation);
+//   }  
+// })
