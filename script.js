7e247bc7843904cfbb3f9d837c907e8bb61837e0
rogerio <rogerio.cl@gmail.com>
2021-03-24 11:52:35 -0300

    asd

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
 
 const formResults = document.getElementsByTagName('input');
 const buttonSubscrive = document.getElementById('facebook-register');
 const result = document.querySelector('.result');
 
-buttonSubscrive.addEventListener('click', function () {
+buttonSubscrive.addEventListener('click', event => {
+  event.preventDefault;
   let contentForm = '';
   for (let index = 0; index < formResults.length; index++) {
       contentForm += `${index} : ${formResults[index].value}; `;      
@@ -12,9 +12,3 @@ buttonSubscrive.addEventListener('click', function () {
     console.log(contentForm);
 })
 result.innerText = window.location.href 
-=======
-function entrarButton () {
-    var emailContent = document.getElementById('user-email-phone').value;
-    alert(emailContent)
-}
->>>>>>> dae006992e0d75c25f3cf78f4b1af5d3cea7c9c0
