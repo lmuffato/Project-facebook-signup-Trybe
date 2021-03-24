a8c57e5acffa99c3094b73927ba1ff3593714871
biazidioti <bzferreira.bio@gmail.com>
2021-03-24 12:23:39 -0300

    Inicia requisito 18.

--- script.js
+++ script.js
@@ -5,12 +5,33 @@ function alert1() {
 const buttonLogin = document.querySelector('#button-login');
 buttonLogin.addEventListener('click', alert1);
 
+
+// criar uma variável q chama o id do form e criar outra variável com os elementos da variável anterior
+const fbForm = document.getElementById('facebook-form');
+const arrayImputs = fbForm.elements;
+const span = document.createElement('span');
+
+// criar função com for para passar por todos os elementos do arrayImputs, excluindo a posicao do gender
+
 function validateRegister() {
-  const validate = document.querySelectorAll('.validate');
-  if (validate === ' ') {
-    alert("Campos inválidos");
-  }
+  for(index = 0; index < arrayImputs.length; index += 1) {
+    if (arrayImputs[index].value === '' && !arrayImputs[8].value) {
+      event.preventDefault();
+      span.innerHTML = 'Campos inválidos';
+      facebookForm.appendChild(span);
+      return true;
+    }
+  } return false;
+
 }
 
-const buttonRegister = document.querySelector('#facebook-register');
-buttonRegister.addEventListener('click', validateRegister);
+// function validateRegister() {
+//   const validate = document.querySelectorAll('.validate');
+//   if (validate === ' ') {
+//     alert("Campos inválidos");
+//   }
+// }
+// const buttonRegister = document.querySelector('#facebook-register');
+// buttonRegister.addEventListener('click', validateRegister);
+
+// var x = document.querySelector("#facebook-form").elements.length;
