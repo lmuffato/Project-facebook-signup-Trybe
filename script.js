631ac9da5166912bf3d544b7c25574f736d5e4a9
Vinicios Fraga <soicinivt@gmail.com>
2021-03-24 11:50:27 -0300

    Requisito 20. substitui o valor do rightContainer pelo valor dos inputs do mesmo.

--- script.js
+++ script.js
@@ -5,18 +5,42 @@ btnLogin.addEventListener('click', () => {
   alert(userEmail.value);
 });
 
+const validate = () => {
+  const rightContent = document.querySelector('.right-content');
+  const name = document.querySelector('#firstName').value;
+  const lastN = document.querySelector('#lastName').value;
+  const phoneMail = document.querySelector('#phoneEmail').value;
+  const birth = document.querySelector('#birthdate').value;
+  const gender = document.querySelectorAll('.genderOptions');
+  let genderValue = '';
+  for (let index = 1; index < gender.length; index += 1) {
+    if (gender[index].checked) {
+      genderValue = gender[index].value;
+    }
+  }
+  rightContent.innerText = `Olá ${name} ${lastN}
+  ${phoneMail}
+  ${birth}
+  ${genderValue}`;
+};
+
 const btnRegister = document.querySelector('#facebook-register');
 btnRegister.addEventListener('click', (event) => {
   const userInputs = document.querySelectorAll('.user-data');
   event.preventDefault();
+  let isValidate = 0;
   for (let index = 0; index < userInputs.length; index += 1) {
     if (userInputs[index].value === '') {
       const formAlert = document.querySelector('.create-acount');
       const spanAlert = document.createElement('span');
       spanAlert.innerText = 'Campos inválidos';
       formAlert.appendChild(spanAlert);
+      isValidate += 1;
       break;
     }
+  }
+  if (isValidate === 0) {
+    return validate();
   }
 });
 
