12a52bc45b3e23c1fe94ab954c185a4ec06d5cf2
Nathalia Zebral <natzebral@gmail.com>
2021-03-24 11:30:30 -0300

    inicio do requisito 18. validacao dos campos preenchidos e mensagem de erro

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ function alertLogin() {
 
 const buttonLogin = document.getElementById('button-login');
 buttonLogin.addEventListener('click', alertLogin);
+
+const formInputs = document.querySelectorAll('.form-inputs');
+const fbForm = document.getElementById('fb-form');
+const createP = document.createElement('p');
+const buttonRegister = document.getElementById('facebook-register');
+
+function validateFields(event) {
+  for (let index = 0; index < formInputs.length; index += 1) {
+    if (formInputs[index].value === '') {
+      event.preventDefault();
+      createP.innerHTML = 'Campos inválidos';
+      fbForm.appendChild(createP);
+      return true;
+    }
+  }
+  return false;
+}
+
+buttonRegister.addEventListener('click', validateFields);
