f12b40cf34f0e98d30ba0ddbb20631fb30720739
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-03-23 22:43:18 -0300

    Requisito 19

--- script.js
+++ script.js
@@ -5,5 +5,35 @@ function enterButton() {
   }
 }
 
+const radio = document.getElementById('gender');
+const customGenderId = 'custom-gender';
+
+function customGender() {
+  const newField = document.createElement('input');
+  const hasField = document.getElementById(customGenderId);
+  if (hasField === null) {
+    newField.id = customGenderId;
+    newField.name = 'gender-custom';
+    newField.placeholder = 'Gênero (opcional)';
+    radio.appendChild(newField);
+  }
+}
+
+function removeCustomField() {
+  const hasField = document.getElementById(customGenderId);
+  if (hasField !== null) {
+    radio.removeChild(hasField);
+  }
+}
+
 const button = document.getElementById('button-login');
 button.addEventListener('click', enterButton);
+
+const personal = document.getElementById('personalizado');
+personal.addEventListener('click', customGender);
+
+const genderMale = document.getElementById('masculino');
+genderMale.addEventListener('click', removeCustomField);
+
+const genderFemale = document.getElementById('feminino');
+genderFemale.addEventListener('click', removeCustomField);
