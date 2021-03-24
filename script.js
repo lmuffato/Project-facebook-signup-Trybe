b8d1d3b36210b14b8d4d0b1a8ab39b8b0b5d11f0
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-03-24 19:59:08 -0300

    Adiciona requisito 18 com correções do lint

--- script.js
+++ script.js
@@ -4,19 +4,24 @@ document.querySelector('#button-login').addEventListener('click', () => {
 });
 
 const buttonCadastro = document.getElementById('facebook-register');
-const newFields = document.querySelector(".fields");
+const newFields = document.querySelector('.fields');
+
+function stopDefAction(evt) {
+  evt.preventDefault();
+}
 
 window.onload = function init() {
   buttonCadastro.addEventListener('click', () => {
-    const arrayValues = [newFields[0].value, newFields[1].value, newFields[2].value, newFields[4].value, newFields[8].value];
+    const arrayValues = [newFields[0].value, newFields[1].value,
+      newFields[2].value, newFields[4].value, newFields[8].value];
     for (let index = 0; index < arrayValues.length; index += 1) {
-      if (arrayValues[index] === "") {
-        let element = document.createElement("p");
-        let form = document.getElementById("collectName");
-        element.innerText = "Campos inválidos";
+      if (arrayValues[index] === '') {
+        const element = document.createElement('p');
+        const form = document.getElementById('collectName');
+        element.innerText = 'Campos inválidos';
         form.appendChild(element);
-        evt.preventDefault();
+        stopDefAction();
       }
     }
-  })
-}
+  });
+};
