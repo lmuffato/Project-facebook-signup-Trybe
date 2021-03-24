55ab70469c25b0ce00fe3280a3e231ef6f41bfce
Stanrley Barbosa <3011stan@gmail.com>
2021-03-24 20:13:06 -0300

    Requisito 18 correcao 2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const getButtonEntrar = document.querySelector('#button-login');
-const fields = document.querySelectorAll("[required]");
+const fields = document.querySelectorAll('[required]');
 const getButtonRegister = document.querySelector('#facebook-register');
 function spamAlert() {
   alert(document.querySelector('#user-email-phone').value);
@@ -7,14 +7,14 @@ function spamAlert() {
 function customValidation() {
   const divInvalid = document.querySelector('#invalid-input');
   const insertText = document.createElement('p');
-  if (document.querySelectorAll('.div-invalida').length > 0){
-  }else {
-  insertText.innerText = 'Campos inválidos';
-  divInvalid.appendChild(insertText);
-  insertText.classList.add('div-invalida');
-}
+  if (document.querySelectorAll('.div-invalida').length > 0) {
+  } else {
+    insertText.innerText = 'Campos inválidos';
+    divInvalid.appendChild(insertText);
+    insertText.classList.add('div-invalida');
+  }
 }
 getButtonEntrar.addEventListener('click', spamAlert);
-for(field of fields) {
+for (field of fields) {
   field.addEventListener("invalid", customValidation);
 }
