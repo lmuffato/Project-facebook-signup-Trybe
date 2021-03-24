28610d5924bb222b547fff23d47944a3aa2dd395
lucas-chave <lcchavesr150@gmail.com>
2021-03-23 22:19:05 -0300

    adicionando requisito 19

--- script.js
+++ script.js
@@ -1,33 +1,22 @@
 const button = document.getElementById('button-login');
 const textInput = document.getElementById('user-email-phone');
 const radioButton = document.getElementById('custom');
-const label = document.getElementById('label-gender');
 const man = document.getElementById('man');
 const woman = document.getElementById('woman');
+const inputCustom = document.querySelector('.inputNone');
 
 function addInput() {
-	if(radioButton.value) {
-		const genderText = document.createElement('input');
-		genderText.setAttribute('type', 'text');
-		label.appendChild(genderText);
-		genderText.classList.add('customGender');
-	}  
-
+  inputCustom.classList.remove('inputNone');
 }
 
 radioButton.addEventListener('click', addInput);
 
-// function removeInput() {
-// 	const removeGender = document.getElementsByClassName('customGender');
-//   if(man.value) {
-// 		label.removeChild('removeGender'); 
-// 	}  
-// }
+function removeInput() {
+  inputCustom.classList.add('inputNone');
+}
 
-// man.addEventListener('click', removeInput);
-// woman.addEventListener('click', removeInput);
-
-
+man.addEventListener('click', removeInput);
+woman.addEventListener('click', removeInput);
 
 function clickButton() {
   const input = textInput.value;
