333620c9b5a92ae5731cbb06f1c7fbc38694ee69
Lucas Muffato <lmiremuffato@gmail.com>
2021-03-24 14:57:14 -0300

    finalização do projeto

--- script.js
+++ script.js
@@ -1,9 +1,104 @@
 const genderCustomConst = 'gender-custom';
+const idSuccessButton = document.querySelector('#facebook-register');
+const inputsText = document.querySelectorAll('.createUser input');
+const radioElement = document.form1;
+const idMessageInvalid = document.querySelector('#messageInvalid');
+const idSucessRegister = document.querySelector('#successRegister');
+const form = document.querySelector('.createUser');
+const register = document.querySelector('.register');
+const quickEasy = document.querySelector('.quick-easy');
+
+const generateInvalidMessage = () => {
+  idMessageInvalid.innerHTML = '';
+  const newMsg = document.createElement('p');
+  newMsg.innerHTML = 'Campos inválidos';
+  idMessageInvalid.appendChild(newMsg);
+};
+
+const validRadio = () => {
+  if (radioElement.gender[0].checked === false
+  && radioElement.gender[1].checked === false
+  && radioElement.gender[2].checked === false) {
+    return false;
+  }
+  return true;
+};
+
+const validTextField = (el) => {
+  if ((el.type === 'text' || el.type === 'password') && el.value === '') {
+    return false;
+  }
+};
+
+const createPGender = () => {
+  if (form.gender[0].checked) {
+    return 'Feminino';
+  }
+  if (form.gender[1].checked) {
+    return 'Masculino';
+  }
+  if (form.gender[2].checked) {
+    return 'Personalizado';
+  }
+};
+
+const toggleDiv = () => {
+  if (form.style.visibility === '') {
+    register.style.display = 'none';
+    quickEasy.style.display = 'none';
+    form.style.display = 'none';
+    idSucessRegister.style.display = 'flex';
+  } else {
+    register.style.display = 'block';
+    quickEasy.style.display = 'block';
+    form.style.display = 'flex';
+    idSucessRegister.style.display = 'none';
+  }
+};
+
+const showNewUser = () => {
+  const nameUser = form.firstname.value;
+  const lastNameUser = form.lastname.value;
+  const phoneEmail = form.phone_email.value;
+  const birthdate = form.birthdate.value;
+
+  idSucessRegister.innerHTML = '';
+
+  idSucessRegister.innerHTML += `
+    <h3>Olá, <strong>${nameUser} ${lastNameUser}</strong></h3>
+  `;
+  idSucessRegister.innerHTML += `<p><b>Telefone/Email: </b> ${phoneEmail}</p>`;
+  idSucessRegister.innerHTML += `
+    <p><b>Data de Nascimento: </b> ${birthdate}</p>
+  `;
+  idSucessRegister.innerHTML += `<p><b>Gênero: </b>${createPGender()}</p>`;
+  toggleDiv();
+};
+
+const validateForm = (e) => {
+  e.preventDefault();
+  let check = true;
+  inputsText.forEach((el) => {
+    if (validTextField(el) === false) {
+      check = false;
+    }
+  });
+  if (!validRadio()) {
+    check = false;
+  }
+  if (check === false) {
+    generateInvalidMessage();
+  } else {
+    idMessageInvalid.innerHTML = '';
+    showNewUser();
+  }
+};
 
 window.onload = () => {
   document.querySelector('#button-login').addEventListener('click', () => {
     alert(document.querySelector('#user-email-phone').value);
   });
+  idSuccessButton.addEventListener('click', validateForm);
 };
 
 function genderCustom() {
@@ -20,9 +115,7 @@ function genderCustom() {
   }
 }
 
-function Clicou1(evento) {
-  console.log('mouse clicou no botão 1');
-  console.log(evento.target);
+function Clicou1() {
   genderCustom();
 }
 
