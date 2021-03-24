bf0fa1d8c346d4e10aa4614c0fc55d947012a03c
rogerio <rogerio.cl@gmail.com>
2021-03-24 14:03:20 -0300

    criação de função de recupera os dados do form e ajustes no html para incluir o radio control

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-
-const formResults = document.getElementsByTagName('input');
+// Bloco de declaração de variáveis globais
+const formResults = document.querySelectorAll('.form-new input');
 const buttonSubscrive = document.getElementById('facebook-register');
 const result = document.querySelector('.result');
+// const buttonLogin = document.getElementById('button-login');
 
-buttonSubscrive.addEventListener('click', event => {
-  event.preventDefault;
-  let contentForm = '';
-  for (let index = 0; index < formResults.length; index++) {
-      contentForm += `${index} : ${formResults[index].value}; `;      
-  }    
-    console.log(contentForm);
-})
-result.innerText = window.location.href 
+// Bloco de declaração de funções
+const isRadioChecked = (index) => {
+  let checked = false;
+  if (formResults[index].type === 'radio' && formResults[index].checked) {
+    checked = true;
+  }
+  return checked;
+};
+
+buttonSubscrive.addEventListener('click', (event) => {
+  event.preventDefault();
+  const contentForm = {};
+  for (let index = 0; index < formResults.length; index += 1) {
+    if (isRadioChecked(index)) {
+      contentForm[formResults[index].name] = formResults[index].value
+    } else {
+      contentForm[formResults[index].name] = formResults[index].value;
+    }
+  }
+  console.log(contentForm);
+});
+
+// bloco de ações de inicialização
+
+result.innerText = window.location.href;
