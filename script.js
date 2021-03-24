e57d22e175647fd9aceef92fdd12d8e4dc762212
rogerio <rogerio.cl@gmail.com>
2021-03-24 11:19:54 -0300

    teste de validação dos dados

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const formResults = document.getElementsByTagName('input');
+const buttonSubscrive = document.getElementById('facebook-register');
+const result = document.querySelector('.result');
+
+buttonSubscrive.addEventListener('click', function () {
+  let contentForm = '';
+  for (let index = 0; index < formResults.length; index++) {
+      contentForm += `${index} : ${formResults[index].value}; `;      
+  }    
+    console.log(contentForm);
+})
+result.innerText = window.location.href 
