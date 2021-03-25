e3726874fc6442cd346c46cee539f2802c25a3f9
Luiz Otavio <luiz.otavio273@gmail.com>
2021-03-25 19:07:31 -0300

    Resolvendo os problemas do eslint

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 const inputsZone = document.querySelectorAll('[required]');
-for (const zone of inputsZone) {
+
+for (let zone of inputsZone) {
   zone.addEventListener('invalid', msgInvalid);
 }
-function msgInvalid (event) {
-  const zone = event.target;
-  function errors () {
+
+function msgInvalid(event) {
+  let zone = event.target;
+  
+  function errors() {
     let erro = false;
-    for (const key in zone.validity) {
-      if (key != 'customError' && zone.validity[key]) {
+    
+    for (let key in zone.validity) {
+      if (key !== 'customError' && zone.validity[key]) {
         erro = true;
       }
     }
     return erro;
   }
-  const found = errors()
+
+  let found = errors()
+  
   if (found) {
-    errors ();
+    errors();
     zone.setCustomValidity('Campos inválidos');
   } else {
     zone.setCustomValidity('');
