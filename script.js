59be70f5fdd0c878701461a24cfe2e6dab201132
Eicke Yumoto <eickeyumoto@gmail.com>
2021-03-24 21:34:03 -0300

    gender correção

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function alerta() {
 }
 
 login.addEventListener('click', alerta);
+
+const buttom = document.getElementById("facebook-register")
+const inputs = document.getElementsByClassName("input")
+const rightContent = document.getElementsByClassName("right-content")
+
+function register() {
+  if (inputs.value = '') {
+    const error = document.createElement('h1')
+    error.innerTEXT = 'Campos inválidos'
+    rightContent.appendChild(error)
+  }
+}
+
+buttom.addEventListener('click', register())
