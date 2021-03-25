bd7a3de115445d76b15a2d9e7f001b1c5ac5a670
Sérgio Martins <sergiomos2010@gmail.com>
2021-03-25 00:02:32 -0300

    Requisito 18: validando form de cadastro

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
 const email = document.querySelector('#user-email-phone');
-const form = document.querySelector('.facebook-login');
+const loginForm = document.querySelector('.facebook-login');
+const singUpForm = document.querySelector('#singup-form');
+const spanMessage = document.querySelector('#message');
 
-function showEmail() {
+const showEmail = () => {
   alert(email.value);
-}
+};
 
-form.addEventListener('submit', (e) => {
+const isFormEmpty = (form) => {
+  const inputs = form.querySelectorAll('input');
+  const inputValues = [];
+
+  inputs.forEach((input) => {
+    if (input.value.length === 0) {
+      inputValues.push(false);
+    } else {
+      inputValues.push(true);
+    }
+  });
+
+  if (inputValues.filter((element) => element === false).length > 0) {
+    return true;
+  }
+  return false;
+};
+
+const singUpFormValidation = (form) => {
+  const isEmpty = isFormEmpty(form);
+  const errorMessage = 'Campos inválidos';
+  const hasErrorMessage = form.innerHTML.includes(errorMessage);
+
+  if (isEmpty && !hasErrorMessage) {
+    spanMessage.innerHTML = errorMessage;
+  } else if (!isEmpty) {
+    spanMessage.innerHTML = '';
+  }
+};
+
+loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
   showEmail();
+});
+
+singUpForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  singUpFormValidation(singUpForm);
 });
