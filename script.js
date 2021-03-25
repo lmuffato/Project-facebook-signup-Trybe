4e15548aa8c5a1123efc0937b55bfb0fbe6c6b38
Stanrley Barbosa <3011stan@gmail.com>
2021-03-25 09:39:13 -0300

    Requisito 19

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const getButtonEntrar = document.querySelector('#button-login');
 const fields = document.querySelectorAll('[required]');
+const getRadioPerson = document.querySelector('#person');
+
 function spamAlert() {
   alert(document.querySelector('#user-email-phone').value);
 }
@@ -17,3 +19,18 @@ getButtonEntrar.addEventListener('click', spamAlert);
 for (let i = 0; i < fields.length; i += 1) {
   fields[i].addEventListener('invalid', customValidation);
 }
+
+function createGenderCustom() {
+  const divPersonGender = document.querySelector('#person-gender');
+  const newText = document.createElement('input');
+  // const getInputExist = document.querySelector('.input-exist');
+  if (document.querySelectorAll('.input-exist').length === 0) {
+    newText.type = 'text';
+    newText.name = 'gender-custom';
+    newText.placeholder = 'Gênero (opcional)';
+    newText.classList.add('input-exist');
+    divPersonGender.appendChild(newText);
+  }
+}
+
+getRadioPerson.addEventListener('click', createGenderCustom);
