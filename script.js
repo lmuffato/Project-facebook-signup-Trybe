2575e52162db9278c17130cadb7cc86fb6ad505b
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-25 19:32:43 -0300

    adicionando o requisito 18

--- script.js
+++ script.js
@@ -14,9 +14,33 @@ function createCustomGender() {
   const genderCustom = document.querySelector('#custom-gender');
   if (personalizado.checked === true) {
     genderCustom.setAttribute('type', 'text');
+    genderCustom.value = '';
   } else {
     genderCustom.setAttribute('type', 'hidden');
   }
 }
 
 createCustomGender();
+
+/* ----- function validate inputs ----- */
+const registerButton = document.querySelector('#facebook-register');
+const form = document.querySelector('#form');
+
+function validate(e){
+  e.preventDefault();
+  const arrayInputs = document.querySelectorAll('.is-filled');
+  let validateCheck = 0;
+
+  for (let index = 0; index < arrayInputs.length; index += 1) {
+    if (arrayInputs[index].value === '' ) {
+      validateCheck += 1;
+    } 
+  }
+  if ( validateCheck !== 0) {
+    const errorMessage = document.createElement('p');
+    errorMessage.innerHTML = 'Campos inválidos';
+    form.appendChild(errorMessage);
+  }
+}
+
+registerButton.addEventListener('click', validate);
