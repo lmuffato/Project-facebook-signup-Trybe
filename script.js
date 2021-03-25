0617013055fb77bbc5e690460b09bb518a3a1121
Sérgio Martins <sergiomos2010@gmail.com>
2021-03-24 22:26:53 -0300

    arrumando lint

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const email = document.querySelector('#user-email-phone');
 const form = document.querySelector('.facebook-login');
-const genders = document.getElementsByName('gender');
 
 function showEmail() {
   alert(email.value);
@@ -10,9 +9,3 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   showEmail();
 });
-
-window.onload => () {
-  genders.forEach((gender) => {
-    console.log(gender)
-  })
-}
