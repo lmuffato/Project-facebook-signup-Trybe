1c82b13c9907f2d6b9b01f38cef10fa1561f597b
walvesb65 <willian.alvesb65@gmail.com>
2021-03-24 22:25:15 -0300

    Requisito 8

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('button-login');
+function alertaButton() {
+const referencia = document.getElementById('user-email-phone').value
+ alert(referencia)
+}
+button.addEventListener('click', alertaButton);
