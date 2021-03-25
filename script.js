2ebc2a6c728d4b410dd6ec93fe7785a9b0c14feb
Sérgio Martins <sergiomos2010@gmail.com>
2021-03-24 22:24:20 -0300

    Requisito 16: refatorando

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const email = document.querySelector('#user-email-phone');
 const form = document.querySelector('.facebook-login');
+const genders = document.getElementsByName('gender');
 
 function showEmail() {
   alert(email.value);
@@ -9,3 +10,9 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   showEmail();
 });
+
+window.onload => () {
+  genders.forEach((gender) => {
+    console.log(gender)
+  })
+}
