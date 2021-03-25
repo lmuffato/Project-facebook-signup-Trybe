37f9c16ecb5619236e2e944910d9f58c67a81360
Maria Lisboa <marialisboa1515@gmail.com>
2021-03-25 10:06:53 -0300

    requisito 19 quase finalizado

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const emailFone = document.querySelector('#user-email-phone');
 buttonLogin.addEventListener('click', () => {
   window.alert(emailFone.value);
 });
+
+
+function criarInput (chek) {
+const divConteiner = document.getElementById("radio-conteiner");
+if (chek.target.checked === true && document.getElementsByClassName("inputDinamico").length == 0) {
+  const criarIn = document.createElement("input"); 
+  criarIn.setAttribute("name", "gender-custom");
+  criarIn.setAttribute("placeholder", "Gênero (opcional)");
+  criarIn.classList.add("inputDinamico");
+  divConteiner.appendChild(criarIn);
+  }
+}
+const btAdicionar = document.getElementById("personalizar");
+btAdicionar.addEventListener("click", function (e) {
+criarInput(e)});
+const btnCadastro = document.getElementById("facebook-register");
+btnCadastro.addEventListener("click", (e) => {
+e.preventDefault()
+});
+
