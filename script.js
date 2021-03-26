3b1f7ec0b13ca7e02788e702bb877b49b71be81d
rogerio <rogerio.cl@gmail.com>
2021-03-25 21:54:38 -0300

    resolvendo o requisto 19

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const formResults = document.querySelectorAll('.form-new input');
 const buttonSubscrive = document.getElementById('facebook-register');
 const result = document.querySelector('.result');
 const selectCustonGender = document.querySelector('#other');
-// const regularGender = document.querySelectorAll('.regular');
+const regularGender = document.querySelectorAll('.regular');
 const resumeForm = document.getElementById('resume-form');
+const subscriveForm = document.querySelector('.form-new');
 
 // Bloco de declaração de funções
 
@@ -19,13 +20,12 @@ const checkValues = (contentForm) => {
   let isValid = true;
   for (let index = 0; index < values.length; index += 1) {
     if (values[index] === '') {
-      result.innerText = `Campos  
-      inválidos`;
+      result.innerText = 'Campos inválidos';
       isValid = false;
     }
   }
   if (isValid) {
-    document.querySelector('.form-new').classList.add('no-view');
+    subscriveForm.classList.add('no-view');
     const resume = `Olá, ${contentForm.firstname} ${contentForm.lastname}!
     Confira seus dados: 
     e-mail ou telefone: ${contentForm.phone_email}
@@ -51,21 +51,21 @@ buttonSubscrive.addEventListener('click', (event) => {
 });
 
 selectCustonGender.addEventListener('change', () => {
-  if (selectCustonGender.checked) {
-    console.log('entrou');
-  } else {
-    console.log('saiu');
-  }
+  const inputGender = document.createElement('input');
+  inputGender.type = 'text';
+  inputGender.name = 'gender-custom';
+  inputGender.placeholder = 'Gênero (opcional)';
+  subscriveForm.appendChild(inputGender);
+  subscriveForm.insertBefore(inputGender, buttonSubscrive);
 });
 
-selectCustonGender.addEventListener('select', () => {
-  if (selectCustonGender.checked) {
-    console.log('entrou2');
-  } else {
-    console.log('saiu');
-  }
-});
+const removePersonalGender = () => {
+  console.log('saiu');
+};
 
 // Blodo de declaração de escutadores
+
+regularGender[0].addEventListener('change', removePersonalGender);
+regularGender[1].addEventListener('change', removePersonalGender);
 
 // bloco de ações de inicialização
