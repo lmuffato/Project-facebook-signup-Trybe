cf2e0b5dccae620c50b055b0e2be92abaae3a53e
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-03-26 11:28:20 -0300

    Finished refactoring new user form checking logic

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function validateForm() {
+/* function validateForm() {
   let somaValidacao = 0;
   const inputValidation = document.querySelectorAll('.validation');
   for (let input = 0; input < inputValidation.length; input += 1) {
@@ -18,11 +18,11 @@ function validateForm() {
         const forms = document.querySelector('body > main > div.right-content > div > form');
         forms.appendChild(comunicate);
       }
-    } else {
+    } else { // I'm not sure what behaviors this else captures
       somaValidacao += 1;
     }
   }
-  if (somaValidacao === inputValidation.length - 2) {
+  if (somaValidacao === inputValidation.length - 2) { // Why 2? Is because we have a radio with 3 options? (So 2 of them are unchosen?)
     const toReplace = document.querySelector('body > main > div.right-content > div');
     const rightContent = document.querySelector('body > main > div.right-content');
     const textName = document.createElement('p');
@@ -43,22 +43,48 @@ function validateForm() {
     }
     toReplace.parentElement.removeChild(toReplace);
   }
+} */
+
+function replaceRegisterFormWithGreetingMessage(inputs) {
+  const rightContent = document.querySelector('.right-content');
+  const greeting = document.createElement('p');
+  greeting.innerText = `Olá, 
+    ${inputs.filter((e) => e.name === 'firstname')[0].value} 
+    ${inputs.filter((e) => e.name === 'lastname')[0].value}
+    ${inputs.filter((e) => e.name === 'phone_email')[0].value}
+    ${inputs.filter((e) => e.name === 'birthdate')[0].value}
+    ${inputs.filter((e) => e.name === 'gender' && e.checked)[0].value}`;
+  rightContent.append(greeting);
+  const toReplace = document.querySelector('.to-replace');
+  toReplace.parentElement.removeChild(toReplace);
+}
+function alertUser() {
+  const comunicate = document.createElement('p');
+  comunicate.innerText = 'Campos inválidos';
+  const forms = document.querySelector('.js-form');
+  forms.appendChild(comunicate);
 }
 
-function validateFormR() {
-  const inputs = Array.from(document.querySelectorAll('.validation'));
+function isValidForm(inputs) {
   const isAnyRadioChecked = inputs.filter(
-    (e) => e.type === 'radio').some((e) => e.checked);
-  // The line below works because [null, undefined, NaN, empty string, 0] all get coerced to false
+    (e) => e.type === 'radio',
+  ).some((e) => e.checked);
+  /* The line below works because [null, undefined, NaN, empty string, 0]
+  all get coerced to false when you use the ! operator */
   const areAllNonRadioFilled = inputs.filter(
-    (e) => e.type !== 'radio').every((e) => !e.value);
-  if (isAnyRadioChecked === false || areAllNonRadioFilled === false) {
-    const comunicate = document.createElement('p');
-    comunicate.innerText = 'Campos inválidos';
-    const forms = document.querySelector('.js-form');
-    forms.appendChild(comunicate);
-  }
+    (e) => e.type !== 'radio',
+  ).every((e) => !!e.value);
+  return (isAnyRadioChecked && areAllNonRadioFilled);
+}
 
+function validateFormR(event) {
+  event.preventDefault();
+  const inputs = Array.from(document.querySelectorAll('.validation'));
+  if (!isValidForm(inputs)) {
+    alertUser();
+  } else {
+    replaceRegisterFormWithGreetingMessage(inputs);
+  }
 }
 
 const registerButton = document.querySelector('#facebook-register');
