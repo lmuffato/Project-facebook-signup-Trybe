5b4871e0c6ec337fdb73e6ab30d0fb44cd55ba5d
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-03-26 08:54:19 -0300

    Refactoring script.js

--- script.js
+++ script.js
@@ -1,31 +1,18 @@
-function displayAlert(event) {
-  event.preventDefault();
-  const userEmailBox = document.getElementById('user-email-phone');
-  alert(userEmailBox.value);
-}
-
-window.onload = function init() {
-  document
-    .getElementById('button-login')
-    .addEventListener('click', displayAlert, false);
-};
-
-const registerButton = document.querySelector('#facebook-register');
-registerButton.addEventListener('click', function () {
+function validateForm() {
   let somaValidacao = 0;
   const inputValidation = document.querySelectorAll('.validation');
   for (let input = 0; input < inputValidation.length; input += 1) {
-    if (inputValidation[input].value === '' && registerButton.nextElementSibling === null) {
+    if (inputValidation[input].value === '' && registerButton.nextElementSibling === null) { // What this second part in the checking does?
       const comunicate = document.createElement('p');
       comunicate.innerText = 'Campos inválidos';
       const forms = document.querySelector('body > main > div.right-content > div > form');
       forms.appendChild(comunicate);
       break;
-    } else if (inputValidation[input].type === 'radio') {
+    } else if (inputValidation[input].type === 'radio') { // if it is a type='radio' doesn't it matches the first condition?
       if (inputValidation[input].checked === true) {
         somaValidacao += 1;
-      } else if (input === inputValidation.length - 1 &&
-        registerButton.nextElementSibling === null) {
+      } else if (input === inputValidation.length - 1 // I don't understand this if here
+        && registerButton.nextElementSibling === null) {
         const comunicate = document.createElement('p');
         comunicate.innerText = 'Campos inválidos';
         const forms = document.querySelector('body > main > div.right-content > div > form');
@@ -56,10 +43,28 @@ registerButton.addEventListener('click', function () {
     }
     toReplace.parentElement.removeChild(toReplace);
   }
-});
+}
 
-const customRadio = document.querySelector('#custom');
-customRadio.addEventListener('click', function () {
+function validateFormR() {
+  const inputs = Array.from(document.querySelectorAll('.validation'));
+  const isAnyRadioChecked = inputs.filter(
+    (e) => e.type === 'radio').some((e) => e.checked);
+  // The line below works because [null, undefined, NaN, empty string, 0] all get coerced to false
+  const areAllNonRadioFilled = inputs.filter(
+    (e) => e.type !== 'radio').every((e) => !e.value);
+  if (isAnyRadioChecked === false || areAllNonRadioFilled === false) {
+    const comunicate = document.createElement('p');
+    comunicate.innerText = 'Campos inválidos';
+    const forms = document.querySelector('.js-form');
+    forms.appendChild(comunicate);
+  }
+
+}
+
+const registerButton = document.querySelector('#facebook-register');
+registerButton.addEventListener('click', validateFormR);
+
+function customGenderBox() {
   if (registerButton.previousElementSibling.name !== 'gender-custom') {
     const newInput = document.createElement('input');
     newInput.type = 'text';
@@ -67,8 +72,25 @@ customRadio.addEventListener('click', function () {
     newInput.placeholder = 'Gênero';
     newInput.className = 'validation';
     newInput.required = 'required';
-    const forms = document.querySelector('body > main > div.right-content > div > form');
+    const forms = document.querySelector(
+      'body > main > div.right-content > div > form',
+    );
     forms.appendChild(newInput);
     forms.insertBefore(newInput, registerButton);
   }
-});
+}
+
+const customRadio = document.querySelector('#custom');
+customRadio.addEventListener('click', customGenderBox);
+
+function displayAlert(event) {
+  event.preventDefault();
+  const userEmailBox = document.getElementById('user-email-phone');
+  alert(userEmailBox.value);
+}
+
+window.onload = function init() {
+  document
+    .getElementById('button-login')
+    .addEventListener('click', displayAlert, false);
+};
