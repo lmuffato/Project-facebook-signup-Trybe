4df3ba59b5ff591bd547744ad5bb03dd61fb7e05
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-03-26 14:15:54 -0300

    Removed "legacy" code and fixed syntax exception problem

--- script.js
+++ script.js
@@ -1,50 +1,3 @@
-/* function validateForm() {
-  let somaValidacao = 0;
-  const inputValidation = document.querySelectorAll('.validation');
-  for (let input = 0; input < inputValidation.length; input += 1) {
-    if (inputValidation[input].value === '' && registerButton.nextElementSibling === null) { // What this second part in the checking does?
-      const comunicate = document.createElement('p');
-      comunicate.innerText = 'Campos inválidos';
-      const forms = document.querySelector('body > main > div.right-content > div > form');
-      forms.appendChild(comunicate);
-      break;
-    } else if (inputValidation[input].type === 'radio') { // if it is a type='radio' doesn't it matches the first condition?
-      if (inputValidation[input].checked === true) {
-        somaValidacao += 1;
-      } else if (input === inputValidation.length - 1 // I don't understand this if here
-        && registerButton.nextElementSibling === null) {
-        const comunicate = document.createElement('p');
-        comunicate.innerText = 'Campos inválidos';
-        const forms = document.querySelector('body > main > div.right-content > div > form');
-        forms.appendChild(comunicate);
-      }
-    } else { // I'm not sure what behaviors this else captures
-      somaValidacao += 1;
-    }
-  }
-  if (somaValidacao === inputValidation.length - 2) { // Why 2? Is because we have a radio with 3 options? (So 2 of them are unchosen?)
-    const toReplace = document.querySelector('body > main > div.right-content > div');
-    const rightContent = document.querySelector('body > main > div.right-content');
-    const textName = document.createElement('p');
-    textName.innerText = `Olá, ${inputValidation[0].value} ${inputValidation[1].value}`;
-    rightContent.append(textName);
-    const textEmail = document.createElement('p');
-    textEmail.innerText = inputValidation[2].value;
-    rightContent.append(textEmail);
-    const textDate = document.createElement('p');
-    textDate.innerText = inputValidation[7].value;
-    rightContent.append(textDate);
-    for (let input = 4; input <= 6; input += 1) {
-      if (inputValidation[input].checked) {
-        const textGender = document.createElement('p');
-        textGender.innerText = inputValidation[input].value;
-        rightContent.append(textGender);
-      }
-    }
-    toReplace.parentElement.removeChild(toReplace);
-  }
-} */
-
 function replaceRegisterFormWithGreetingMessage(inputs) {
   const rightContent = document.querySelector('.right-content');
   const greeting = document.createElement('p');
@@ -77,7 +30,7 @@ function isValidForm(inputs) {
   return (isAnyRadioChecked && areAllNonRadioFilled);
 }
 
-function validateFormR(event) {
+function validateForm(event) {
   event.preventDefault();
   const inputs = Array.from(document.querySelectorAll('.validation'));
   if (!isValidForm(inputs)) {
@@ -87,10 +40,8 @@ function validateFormR(event) {
   }
 }
 
-const registerButton = document.querySelector('#facebook-register');
-registerButton.addEventListener('click', validateFormR);
-
 function customGenderBox() {
+  const registerButton = document.querySelector('#facebook-register');
   if (registerButton.previousElementSibling.name !== 'gender-custom') {
     const newInput = document.createElement('input');
     newInput.type = 'text';
@@ -106,9 +57,6 @@ function customGenderBox() {
   }
 }
 
-const customRadio = document.querySelector('#custom');
-customRadio.addEventListener('click', customGenderBox);
-
 function displayAlert(event) {
   event.preventDefault();
   const userEmailBox = document.getElementById('user-email-phone');
@@ -119,4 +67,8 @@ window.onload = function init() {
   document
     .getElementById('button-login')
     .addEventListener('click', displayAlert, false);
+  const registerButton = document.querySelector('#facebook-register');
+  registerButton.addEventListener('click', validateForm);
+  const customRadio = document.querySelector('#custom');
+  customRadio.addEventListener('click', customGenderBox);
 };
