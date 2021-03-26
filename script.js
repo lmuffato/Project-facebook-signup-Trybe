f62ae10d69aa7e7abc0aacdb35112f1f538e61e2
GetulioGutemberg <getulio.dev@gmail.com>
2021-03-25 21:30:20 -0300

    resolve requisito 20

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const buttonLogin = document.getElementById('button-login');
 const inputEmailTel = document.getElementById('user-email-phone');
 const submitButton = document.getElementById('facebook-register');
 const openAccInput = document.getElementsByClassName('openAccount');
-
 buttonLogin.addEventListener('click', (Event) => {
   Event.preventDefault();
   alert(inputEmailTel.value);
@@ -25,23 +24,36 @@ function checkNullInputRadio() {
   }
   return checkers;
 }
+function boasVindas() {
+  const divpai = document.getElementsByClassName('right-content');
+  const filhos = divpai[0].children;
+  const dados = [];
+  dados[0] = [openAccInput[0].value];
+  dados[1] = [openAccInput[1].value];
+  dados[2] = [openAccInput[2].value];
+  dados[3] = [openAccInput[4].value];
+  for (let index = filhos.length - 1; index >= 0; index -= 1) {
+    divpai[0].removeChild(filhos[index]);
+  }
+  const alerta = document.createElement('h3');
+  alerta.innerText = `Olá, ${dados[0]}  ${dados[1]} ,
+  ${dados[2]}
+  ${dados[3]} .`;
+  alerta.setAttribute('class', 'welcome');
+  divpai[0].appendChild(alerta);
+}
 function formData() {
   const checkerText = checkNullInputText();
   const checkerRadio = checkNullInputRadio();
-  
   if (checkerText === true || checkerRadio.length !== 2) {
-    const divform = document.getElementById("registerBox");
+    const divform = document.getElementById('registerBox');
     const alerta = document.createElement('h3');
     alerta.innerText = 'Campos inválidos';
     alerta.setAttribute('class', 'alertlog');
     divform.appendChild(alerta);
   }
   if (checkerText === false || checkerRadio.length === 2) {
-    const divform = document.getElementById("registerBox");
-    const alerta = document.createElement('h3');
-    alerta.innerText = 'Olá, ' + document.getElementById('firstname').value + ' ' + document.getElementById('lastname').value + ',     ' +document.getElementById('emailOrPhone').value;
-    alerta.setAttribute('class', 'welcome');
-    divform.appendChild(alerta);
+    boasVindas();
   }
 }
 submitButton.addEventListener('click', (Event) => {
