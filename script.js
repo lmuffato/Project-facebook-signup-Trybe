e4deeefb30379e51f74fc6357097892d605b0d0d
Henrique Lima <hnamix@gmail.com>
2021-03-27 14:37:09 -0400

    requisito 17

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function mostrarAlert() {
   const emailtel = document.getElementById('user-email-phone');
   alert(emailtel.value);
 }
+
+const botao = document.getElementById('button-login');
+botao.addEventListener('click', mostrarAlert);
