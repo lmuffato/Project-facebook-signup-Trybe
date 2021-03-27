4e3c10bfc5dcc9baca689374b17c9137f11a61ad
Lucas Brito <lucasbrito.carneiro@gmail.com>
2021-03-27 13:24:50 -0300

    Lint

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //  Função click exibe alerta com valor do campo
 function testFunction() {
-    const button = document.querySelector('.form-control');
-    button.addEventListener('click', () => {
-      const text = document.getElementById('user-email-phone').value;
-      alert(text);
-    });
-  }
-  
-  testFunction();
+  const button = document.querySelector('.form-control');
+  button.addEventListener('click', () => {
+    const text = document.getElementById('user-email-phone').value;
+    alert(text);
+  });
+}
+
+testFunction();
