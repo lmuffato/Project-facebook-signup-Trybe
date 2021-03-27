12772cf29dc25193c0f3d72a38dbb6542ae16b59
Henrique Lima <hnamix@gmail.com>
2021-03-27 16:15:43 -0400

    requisito 17

--- script.js
+++ script.js
@@ -5,16 +5,3 @@ function mostrarAlert() {
 
 const botao = document.getElementById('button-login');
 botao.addEventListener('click', mostrarAlert);
-
-/*const btn = document.getElementById('facebook-register');
-const checarseTem = () => {
-  console.log('entrou função');
-  const inputs = document.getElementById('cadastro').elements;
-  for (let index = 0; index < inputs.length; index += 1) {
-    if (inputs[index].value === undefined) {
-      alert('Campos inválidos');
-      return;
-    }
-  }
-};
-btn.addEventListener('click', checarseTem);*/
