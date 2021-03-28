160cba155cfd757bf856488a3e7accfd939ae869
Douglas <douguinhodns@gmail.com>
2021-03-28 19:10:24 -0300

    preparando para pull request

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const loginBut = document.getElementById('button-login');
-
 const nameValue = document.getElementById('user-email-phone');
 
 function alertBut() {
@@ -7,3 +6,21 @@ function alertBut() {
 }
 
 loginBut.addEventListener('click', alertBut);
+
+// Requisito cumprido com a ajuda do fantástico instrutor John, no plantão DOMINGO!!!
+
+function displayInput() {
+  const radios = document.querySelectorAll('.select');
+  const personalizado = document.querySelector('#custom');
+  const displayInputCSS = document.querySelector('#customInput');
+  for (let index = 0; index < radios.length; index += 1) {
+    if (personalizado.checked) {
+      displayInputCSS.style.display = 'block';
+    } else {
+      displayInputCSS.style.display = 'none';
+    }
+  }
+}
+
+const radiosDiv = document.querySelector('#gender-options');
+radiosDiv.addEventListener('click', displayInput);
