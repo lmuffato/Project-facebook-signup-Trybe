1ed3c13a1021a8628fbf01385ddc8efc46e26e29
jonathan <jonathanfernandesjf98@gmail.com>
2021-03-27 23:27:02 -0300

    requisito 18

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const inputsValid = document.getElementsByClassName('inputs');
+const divAlert = document.getElementById('divInvalid');
+const createP = document.createElement('p');
+
 function alertFunction() {
   const input = document.getElementById('user-email-phone');
   alert(input.value);
@@ -8,3 +12,33 @@ function alertButtonLogin() {
   buttonLogin.addEventListener('click', alertFunction);
 }
 alertButtonLogin();
+
+function validateInputs(event) {
+  for (let index = 0; index < inputsValid.length; index += 1) {
+    if (inputsValid[index].value === '') {
+      event.preventDefault();
+      divAlert.appendChild(createP).innerText = 'Campos inválidos';
+    }
+    if (divAlert.children.length > 1) {
+      divAlert.removeChild(createP);
+    }
+  }
+}
+function validateRadioButtons(event) {
+  const inputsGenders = document.getElementsByClassName('inputsGender');
+  if (inputsGenders.checked[0, 1, 2] === false) {
+    event.preventDefault();
+    divAlert.appendChild(createP).innerText = 'Campos inválidos';
+  }
+  if (divAlert.children.length > 1) {
+    divAlert.removeChild(createP);
+  }
+}
+
+function submitButton() {
+  const buttonSubmit = document.getElementById('facebook-register');
+  buttonSubmit.addEventListener('click', validateInputs);
+  buttonSubmit.addEventListener('click', validateRadioButtons);
+}
+
+submitButton();
