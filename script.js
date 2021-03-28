4f6f7e01501c986da4413cce07bda33207c18cbc
Gabriel Ferreira <gabrielferreira@pop-os.localdomain>
2021-03-28 19:25:03 -0300

    Refazendo requisito 19

--- script.js
+++ script.js
@@ -4,77 +4,87 @@ const otherGenders = document.getElementById('other');
 const formRegister = document.getElementById('form-register');
 const btnRegister = document.getElementById('facebook-register');
 const genders = document.getElementById('genders');
-const inputsForm = document.querySelectorAll('.input');
-const messageError = document.querySelector('#messageError');
-const newAccount = document.querySelector('.new-account');
-const rightContent = document.querySelector('.right-content');
-const mainContent = document.querySelector('.main-content');
-const paragrafo = document.createElement('p');
-const div = document.createElement('div');
+let genderValue = '';
+// const inputsForm = document.querySelectorAll('.input');
+// const messageError = document.querySelector('#messageError');
+// const newAccount = document.querySelector('.new-account');
+// const rightContent = document.querySelector('.right-content');
+// const mainContent = document.querySelector('.main-content');
+// const paragrafo = document.createElement('p');
+// const div = document.createElement('div');
 
 btnLogin.addEventListener('click', () => {
   alert(emailPhone.value);
 });
 
-const genderInput = () => {
-  const gender = document.createElement('input');
-  gender.type = 'text';
-  gender.id = 'gender-custom';
-  gender.name = 'gender-custom';
-  gender.placeholder = 'Gênero (opcional)';
-  if (otherGenders) {
-    genders.appendChild(gender);
-  }
-  otherGenders.removeEventListener('click', genderInput);
-};
+// const genderInput = () => {
+//   const gender = document.createElement('input');
+//   gender.type = 'text';
+//   gender.id = 'gender-custom';
+//   gender.name = 'gender-custom';
+//   gender.placeholder = 'Gênero (opcional)';
+//   if (otherGenders) {
+//     genders.appendChild(gender);
+//   }
+// };
 
-const checkRadios = () => {
-  let inputChecked = 0;
-  const gendersInput = document.querySelectorAll('#genders label input');
-  for (let i = 0; i < gendersInput.length; i += 1) {
-    if (gendersInput[i].checked) {
-      inputChecked += 1;
-    }
-  }
-  return inputChecked;
-};
+// const checkRadios = () => {
+//   let inputChecked = 0;
+//   const gendersInput = document.querySelectorAll('#genders label input');
+//   for (let i = 0; i < gendersInput.length; i += 1) {
+//     if (gendersInput[i].checked) {
+//       inputChecked += 1;
+//     }
+//   }
+//   return inputChecked;
+// };
 
-const checkEmpty = () => {
-  const formInputs = formRegister.querySelectorAll('input');
-  const errorMessage = document.getElementById('message-error');
+// const checkEmpty = () => {
+//   const formInputs = formRegister.querySelectorAll('input');
+//   const errorMessage = document.getElementById('message-error');
 
-  for (let i = 0; i < formInputs.length; i += 1) {
-    if (formInputs[i].value === '' || checkRadios() === 0) {
-      errorMessage.innerHTML = 'Campos inválidos';
-    } else {
-      errorMessage.innerHTML = '';
-    }
+//   for (let i = 0; i < formInputs.length; i += 1) {
+//     if (formInputs[i].value === '' || checkRadios() === 0) {
+//       errorMessage.innerHTML = 'Campos inválidos';
+//     } else {
+//       errorMessage.innerHTML = '';
+//     }
+//   }
+// };
+
+genders.addEventListener('click', (e) => {
+  const inputSelected = e.target;
+  const customGenderInput = document.getElementById('custom-gender');
+  if (inputSelected.value === 'Personalizado') {
+    customGenderInput.style.display = 'block';
+  } else {
+    customGenderInput.style.display = 'none';
   }
-};
+  genderValue = inputSelected.value;
+});
 
 formRegister.addEventListener('submit', (e) => {
   e.preventDefault();
 });
 
-otherGenders.addEventListener('click', genderInput);
 btnRegister.addEventListener('click', () => {
-  checkEmpty();
+  // checkEmpty();
 });
 
-function validaCampos(evento) {
-  evento.preventDefault();
-  for (let i = 0; i < inputsForm.length; i += 1) {
-    const generoSelec = document.querySelector('input[name="gender"]:checked');
-    if (inputsForm[i].value === '' || generoSelec === null) {
-      messageError.innerHTML = 'Campos inválidos';
-    } else {
-      paragrafo.innerHTML = `Olá, ${inputsForm[0].value} ${inputsForm[1].value}
-      ${inputsForm[2].value}, ${inputsForm[4].value}, ${generoSelec.value}`;
-      mainContent.removeChild(rightContent);
-      mainContent.appendChild(div);
-      div.className = 'right-content';
-      div.appendChild(paragrafo);
-    }
-  }
-}
-newAccount.addEventListener('submit', validaCampos);
+// function validaCampos(evento) {
+//   evento.preventDefault();
+//   for (let i = 0; i < inputsForm.length; i += 1) {
+//     const generoSelec = document.querySelector('input[name="gender"]:checked');
+//     if (inputsForm[i].value === '' || generoSelec === null) {
+//       messageError.innerHTML = 'Campos inválidos';
+//     } else {
+//       paragrafo.innerHTML = `Olá, ${inputsForm[0].value} ${inputsForm[1].value}
+//       ${inputsForm[2].value}, ${inputsForm[4].value}, ${generoSelec.value}`;
+//       mainContent.removeChild(rightContent);
+//       mainContent.appendChild(div);
+//       div.className = 'right-content';
+//       div.appendChild(paragrafo);
+//     }
+//   }
+// }
+// newAccount.addEventListener('submit', validaCampos);
