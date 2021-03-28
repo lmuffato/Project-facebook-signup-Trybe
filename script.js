9bad868ad6c49d31e96bb3b7fe4c0378cb960292
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-28 19:49:38 -0300

    Requisito 18 revisado 14

--- script.js
+++ script.js
@@ -5,13 +5,34 @@ btnEntrar.addEventListener('click', () => alert(userEmailPhone.value));
 
 // Requisito 18
 // Referencia para resolução do requisito : https://www.youtube.com/watch?v=GTMEuHxh8aQ
+// function verifyError(field) {
+//   let foundError = false;
+//   const fieldValidty = Object.values(field.validity);
+
+//   for (let index = 0; index < fieldValidty.length; index += 1) {
+//     if (!'customError' && fieldValidty) {
+//       foundError = true;
+//     }
+//   }
+//   return foundError;
+// }
+
+// function customValidation(event) {
+//   const field = event.target;
+//   const error = verifyError(field);
+
+//   if (error) {
+//     field.setCustomValidity('Campos inválidos');
+//   }
+//   field.setCustomValidity('');
+// }
 
 function eventRequired() {
-  let msg = document.getElementById('msg');
-  let messagem = msg.innerHTML = '<span><strong>Campos inválidos</strong></span>';
+  const msg = document.getElementById('msg');
   const fields = document.querySelectorAll('[required]');
+  const mensagem = '<span><strong>Campos inválidos</strong></span>';
   for (let index = 0; index < fields.length; index += 1) {
-    fields[index].addEventListener('invalid', () => messagem);
+    fields[index].addEventListener('invalid', () => msg.innerHTML = mensagem);
   }
 }
 
