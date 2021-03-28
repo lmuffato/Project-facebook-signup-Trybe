dd57a744e0457fd8a314952de513e63214fd61ba
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-28 14:46:43 -0300

    Requisito 18 revisado 3

--- script.js
+++ script.js
@@ -3,38 +3,34 @@ const btnEntrar = document.getElementById('button-login');
 const userEmailPhone = document.getElementById('user-email-phone');
 btnEntrar.addEventListener('click', () => alert(userEmailPhone.value));
 
-// requisito 18
-// referencia para resolução do requisito : https://www.youtube.com/watch?v=GTMEuHxh8aQ
-function verifYError(field){
-    
-    let foundError = false;
-    for (const key in field.validity) {
-        if (key !== 'customError' && field.validity[key]) {
-            foundError = key ;
-        }
-    } 
+// Requisito 18
+// Referencia para resolução do requisito : https://www.youtube.com/watch?v=GTMEuHxh8aQ
+function verifyError(field) {
+  let foundError = false;
+  for (const key in field.validity) {
+    if (key !== 'customError' && field.validity[key]) {
+      foundError = key ;
+    }
+   } 
    return foundError;
-
 }
 
-function customValidation(event){
-    const field = event.target;
+function customValidation(event) {
+  const field = event.target;
+  const error = verifyError(field);
 
-    const error = verifYError(field);
-
-    if(error){
-        field.setCustomValidity("Campos inválidos");
-    }else{
-        field.setCustomValidity('');
+  if(error) {
+    field.setCustomValidity("Campos inválidos");
     }
+    field.setCustomValidity('');  
 } 
 
-function eventRequired(){
-    const fields = document.querySelectorAll('[required]');
-    for (field of fields ){
-        field.addEventListener('invalid', customValidation)
-    }
+function eventRequired() {
+  const fields = document.querySelectorAll('[required]');
+  for (const field of fields) {
+    field.addEventListener('invalid', customValidation);
+  }
 }
 
 const btncadastra = document.getElementById('facebook-register');
-btncadastra.addEventListener('click',eventRequired);
+btncadastra.addEventListener('click', eventRequired);
