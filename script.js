d35c04a85108e75fc03a580f93d3440edd1ffc57
camiloLelis <lafirmafuerte@gmail.com>
2021-03-28 11:18:54 -0300

    Requisito 18 - revisado 2

--- script.js
+++ script.js
@@ -2,12 +2,3 @@
 const btnEntrar = document.getElementById('button-login');
 const userEmailPhone = document.getElementById('user-email-phone');
 btnEntrar.addEventListener('click', () => alert(userEmailPhone.value));
-
-const btncadastrar = document.getElementById('facebook-register');
-btncadastrar.addEventListener('click', () =>{
-    const inputname = document.getElementsByClassName('lastName');
-    if (inputname.value === " ") {
-        console.log("campo vazio");
-
-    }
-})
