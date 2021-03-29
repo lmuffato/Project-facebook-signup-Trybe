413ca6bfb37c05d6fd20e6b636f389a4f2f0acba
luanlsr <luanfswd@gmail.com>
2021-03-29 09:19:42 -0300

    Requisitos 18 e 19 finalizados

--- script.js
+++ script.js
@@ -5,3 +5,49 @@ function submitCancel(event) {
   alert(`${login}`);
 }
 button.addEventListener('click', submitCancel);
+
+//função feita baseado no vídeo da rocktseat https://www.youtube.com/watch?v=GTMEuHxh8aQ&t=2100s
+const fields = document.querySelectorAll("[required]");
+function validateForms(event) {
+  const field = event.target;
+  console.log('Campo Inválido');
+
+  function verifyErrors(){
+    let foundError = false;
+
+    for (let error in field.validity) {
+      if (error !== 'customError' && field.validity[error]) {
+        foundError = true;
+      }
+    }
+    return foundError;
+  }
+  const error = verifyErrors();
+
+  if (error) {
+    field.setCustomValidity('Campos inválidos');
+  } else {
+    field.setCustomValidity('');
+  }
+}
+
+for (field of fields) {
+  field.addEventListener('invalid', validateForms);
+}
+
+const form = document.querySelector('#form2');
+form2.addEventListener('submit', event => {
+  event.preventDefault();
+})
+
+const personalizado = document.getElementById('radioPersonalizado');
+function radio() {
+  const inputPersonalizado = document.getElementById('inputPersonalizado');
+  inputPersonalizado.style.display = 'flex';
+  inputPersonalizado.style.width = '200px';
+  inputPersonalizado.attributes = 'required';
+  console.log('fui clicado');
+}
+
+personalizado.addEventListener('click', radio);
+
