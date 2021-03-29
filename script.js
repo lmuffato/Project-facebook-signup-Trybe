4753b16d812693467be079dc3d7b9ade69a9e2cc
luanlsr <luanfswd@gmail.com>
2021-03-29 11:10:57 -0300

    finalizado requisito 20

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ button.addEventListener('click', submitCancel);
 const fields = document.querySelectorAll("[required]");
 function validateForms(event) {
   const field = event.target;
-  console.log('Campo Inválido');
+  console.log('Campo inválido');
 
   function verifyErrors(){
     let foundError = false;
@@ -29,32 +29,42 @@ function validateForms(event) {
   } else {
     field.setCustomValidity('');
   }
+  return true
 }
 
 for (field of fields) {
   field.addEventListener('invalid', validateForms);
 }
 
-const form = document.querySelector('#form2');
-form2.addEventListener('submit', event => {
-  event.preventDefault();
-})
+const firstName = document.getElementsByName('firstname')[0];
+const lastName = document.getElementsByName('lastname')[0];
+const phoneEmail = document.getElementsByName('phone_email')[0];
+const birthDate = document.getElementsByName('birthdate')[0];
+const female = document.getElementById('feminino');
+const male = document.getElementById('masculino');
+const customGender = document.getElementById('radioPersonalizado');
+
 //evento concluído baseado neste vídeo sobre eventos com radio buttons https://www.youtube.com/watch?v=zndWZv9DZWc
-const personalizado = document.getElementById('radioPersonalizado');
 function radioPersonalizado() {
   const inputPersonalizado = document.getElementById('inputPersonalizado');
   inputPersonalizado.style.display = 'flex';
   inputPersonalizado.style.width = '200px';
   inputPersonalizado.attributes = 'required';
   console.log('fui clicado');
+  female.classList.remove('checked');
+  male.classList.remove('checked');
+  customGender.classList.add('checked');
 }
 
-personalizado.addEventListener('click', radioPersonalizado);
+customGender.addEventListener('click', radioPersonalizado);
 
 const masculino = document.getElementById('masculino');
 function radioMasculino() {
   const inputPersonalizado = document.getElementById('inputPersonalizado');
   inputPersonalizado.style.display = 'none';
+  female.classList.remove('checked');
+  male.classList.add('checked');
+  customGender.classList.remove('checked');
 }
 
 masculino.addEventListener('click', radioMasculino);
@@ -63,6 +73,67 @@ const feminino = document.getElementById('feminino');
 function radioFeminino() {
   const inputPersonalizado = document.getElementById('inputPersonalizado');
   inputPersonalizado.style.display = 'none';
+  female.classList.add('checked');
+  male.classList.remove('checked');
+  customGender.classList.remove('checked');
 }
 
 feminino.addEventListener('click', radioFeminino);
+//criação de div a partir do submit da página feito baseado no code Review do pull request da dupla Maria Luiza e Cla Oliveira https://github.com/tryber/sd-010-a-project-facebook-signup/pull/104/files
+const content = {
+  firstName: '',
+  lastName: '',
+  phoneEmail: '',
+  birthDate: '',
+  gender: '', 
+};
+
+function createValues() {
+  content.firstName = firstName.value;
+  content.lastName = lastName.value;
+  content.phoneEmail = phoneEmail.value;
+  content.birthDate = birthDate.value;
+
+  if (customGender.classList.contains('checked')) {
+    content.gender = customGender.value;
+  } else if (female.classList.contains('checked')) {
+    content.gender = female.value;
+  } else if (male.classList.contains('checked')) {
+    content.gender = male.value;
+  }
+}
+
+function validate() {
+  const validate = document.querySelectorAll('.validate');
+  let genderValue = 0;
+  for (let index = 0; index < validate.length; index += 1) {
+    if (validate[index].value === '') {
+      return false;
+    }
+  }
+  return true;
+}
+const btnSubmit =  document.getElementById('facebook-register');
+const rightContent = document.querySelector('.right-content');
+function gerarInformacoes(e) {
+  if (validate()) {
+    e.preventDefault();
+
+    createValues();
+
+    rightContent.innerHTML = '';
+    const informations = document.createElement('div');
+
+    const string = `Olá, ${content.firstName} ${content.lastName}
+  ${content.phoneEmail}
+  ${content.birthDate}
+  ${content.gender}`;
+
+    informations.innerText = string;
+    informations.style.marginTop = "50px"
+    rightContent.appendChild(informations);
+  }
+  
+}
+
+btnSubmit.addEventListener('click', gerarInformacoes);
