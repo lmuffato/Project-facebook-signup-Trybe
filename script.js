ceabf609a62fbab169db0b9e808e8756d35e12f7
Robertson <robertson.nunes@gmail.com>
2021-03-29 12:37:33 -0300

    conferir nota

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const user = document.getElementById('user-email-phone');
 const login = document.getElementById('button-login');
+/* const buttom = document.getElementById('facebook-register');
+const inputs = document.getElementsByTagName('input')
+const text = document.createElement('p')
+const formulario = document.getElementById('nameContainer') */
 
 function alerta() {
   alert(user.value);
@@ -7,25 +11,26 @@ function alerta() {
 
 login.addEventListener('click', alerta);
 
-const buttom = document.getElementById('facebook-register');
-const inputs = document.getElementsByClassName('input');
+/* function checkFill {
+  text.innerText = 'Campos inválidos'
+  ifinputs.value === '') {
+    text.appendChild(formulario)
+  }
+}
+buttom.addEventListener('click', checkFill)
+
 const rightContent = document.querySelector('.right-content');
 const error = document.createElement('h1');
 const radio = document.querySelector('#form').gender;
 const submitForm = document.getElementById('submitForm');
 // inputs values/////////////////
 const name = document.getElementById('name').value;
-// eslint-disable-next-line no-unused-vars
 const sobrenome = document.getElementById('sobrenome');
-// eslint-disable-next-line no-unused-vars
 const email = document.getElementById('celular-email');
-// eslint-disable-next-line no-unused-vars
 const senha = document.getElementById('senha');
-// eslint-disable-next-line no-unused-vars
 const birthdate = document.getElementById('birthdate');
 function checkGender() {
   for (let index = 0; index < 3; index += 1) {
-    // eslint-disable-next-line no-empty
     if (radio[index] === true) {
     }
   }
@@ -37,20 +42,20 @@ console.log(radio);
 
 function checkRadio() {
   if (radio[0].checked === false && radio[1].checked === false && radio[2].checked === false) {
-    return false
+    return false;
   }
-return true
+  return true;
 }
 
 function register(e) {
-  e.preventDefault()
+  e.preventDefault();
   for (let index = 0; index < inputs.length; index += 1) {
     if (inputs[index].value === '' || checkRadio() === false) {
       error.innerText = 'Campos inválidos';
       rightContent.appendChild(error);
       break;
     }
-    rightContent.classList.add('removeElement')
+    rightContent.classList.add('removeElement');
     submitedForm();
   }
 }
@@ -61,5 +66,4 @@ function submitedForm() {
   submitForm.classList.remove('removeElement');
   const p = document.getElementById('p');
   p.innerHTML = `Olá ${name}`;
-}
-
+} */
