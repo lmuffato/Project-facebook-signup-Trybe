1110e1e6f2315c0b5cab345ad8086da6f8ef7cdf
Eicke Yumoto <eickeyumoto@gmail.com>
2021-03-31 11:18:18 -0300

    Requisitos 19 e 20

--- script.js
+++ script.js
@@ -9,84 +9,84 @@ function alerta() {
   alert(user.value);
 }
 
-// login.addEventListener('click', alerta);
+login.addEventListener('click', alerta);
 
-// // function checkFill() {
-// //   text.innerText = 'Campos inválidos'
-// //   if (inputs.value === '') {
-// //     text.appendChild(formulario)
-// //   }
-// // }
-// // buttom.addEventListener('click', checkFill)
+const rightContent = document.querySelector('.right-content');
+const error = document.createElement('h1');
+const radio = document.querySelector('#form').gender;
+const submitForm = document.getElementById('submitForm');
+// inputs values/////////////////
+const name = document.getElementById('name');
+const sobrenome = document.getElementById('sobrenome');
+const email = document.getElementById('celular-email');
+const senha = document.getElementById('senha');
+const birthdate = document.getElementById('birthdate');
+function checkGender() {
+  let genderSelected = ''
+  for (let index = 0; index < 3; index += 1) {
+    if (radio[index].checked) {
+      genderSelected = radio[index].value
+    }
+  }
+  return genderSelected
+}
 
-// const rightContent = document.querySelector('.right-content');
-// const error = document.createElement('h1');
-// const radio = document.querySelectorAll('#form').gender;
-// const submitForm = document.getElementById('submitForm');
-// // inputs values/////////////////
-// const name = document.getElementById('name');
-// const sobrenome = document.getElementById('sobrenome');
-// const email = document.getElementById('celular-email');
-// const senha = document.getElementById('senha');
-// const birthdate = document.getElementById('birthdate');
-// function checkGender() {
-//   let genderSelected = ''
-//   for (let index = 0; index < 3; index += 1) {
-//     if (radio[index].checked) {
-//       genderSelected = radio[index].value
-//     }
-//   }
-//   return genderSelected
-// }
-// // checkGender();
-// const genderSelect = checkGender();
-// // inputs values/////////////////
+console.log(radio);
 
-// console.log(radio);
+function checkRadio() {
+  if (radio[0].checked === false && radio[1].checked === false && radio[2].checked === false) {
+    return false;
+  }
+  return true;
+}
 
-// function checkRadio() {
-//   if (radio[0].checked === false && radio[1].checked === false && radio[2].checked === false) {
-//     return false;
-//   }
-//   return true;
-// }
+function register(e) {
+  e.preventDefault();
+  for (let index = 2; index < inputs.length; index += 1) {
+    if (inputs[index].value === '' || checkRadio() === false) {
+      error.innerText = 'Campos inválidos';
+      rightContent.appendChild(error);
+      break;
+    }
+    rightContent.classList.add('removeElement');
+    submitedForm();
+  }
+}
 
-// function register(e) {
-//   e.preventDefault();
-//   for (let index = 2; index < inputs.length; index += 1) {
-//     if (inputs[index].value === '' || checkRadio() === false) {
-//       error.innerText = 'Campos inválidos';
-//       rightContent.appendChild(error);
-//       break;
-//     }
-//     rightContent.classList.add('removeElement');
-//     submitedForm();
-//   }
-// }
+buttom.addEventListener('click', register);
 
-// buttom.addEventListener('click', register);
+function submitedForm() {
+  submitForm.classList.remove('removeElement');
+  const p = document.getElementById('p');
+  p.innerHTML = `Olá ${nome.value} ${sobrenome.value}<br>
+  Email: ${email.value}<br>
+  Data de nascimento: ${birthdate.value}<br>
+  Gênero: ${checkGender()}`
+}
 
-// function submitedForm() {
-//   submitForm.classList.remove('removeElement');
-//   const p = document.getElementById('p');
-//   p.innerHTML = `Olá ${nome.value} ${sobrenome.value}<br>
-//   Email: ${email.value}<br>
-//   Data de nascimento: ${birthdate.value}<br>
-//   Gênero: ${genderSelect}`
-// }
+const girlGender = document.getElementById('Masculino')
+const boyGender = document.getElementById('Feminino')
+const gender = document.getElementById('Personalizado')
+const genderConteiner = document.querySelector('.genderContainer')
 
-// const gender = document.getElementById('personalizado')
-// const genderConteiner = document.getElementsByClassName('genderConteiner')
+function customGender() {
+  const customInput = document.createElement('input')
+  customInput.name = 'gender-custom'
+  customInput.placeholder = 'Gênero (opcional)'
+  customInput.id = 'customId'
+  genderConteiner.appendChild(customInput)
+}
 
-// function customGender() {
-//   const customInput = document.createElement('input')
-//   genderConteiner.appendChild(customInput)
-//   Object.assign(customInput, {
-//     id: customGenderId,
-//     name: customGenderName,
-//     type: 'text',
-//     placeholder: 'Gênero (opcional)',
-//   });
-// }
+gender.addEventListener('click', customGender);
 
-// gender.addEventListener('click', customGender);
+function removeCustomGender() {
+  const customId = document.getElementById('customId')
+  if (customId !== null) {
+    genderConteiner.removeChild(customId)
+  }
+}
+
+girlGender.addEventListener('click', removeCustomGender)
+
+boyGender.addEventListener('click', removeCustomGender)
+
