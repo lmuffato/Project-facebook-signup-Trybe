2c9361adee69a1f09b946f72f49f40cd7c607ba2
Eicke Yumoto <eickeyumoto@gmail.com>
2021-03-30 23:36:25 -0300

    js

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
 const user = document.getElementById('user-email-phone');
 const login = document.getElementById('button-login');
-/* const buttom = document.getElementById('facebook-register');
+const buttom = document.getElementById('facebook-register');
 const inputs = document.getElementsByTagName('input')
 const text = document.createElement('p')
-const formulario = document.getElementById('nameContainer') */
+const formulario = document.getElementById('nameContainer') 
 
 function alerta() {
   alert(user.value);
 }
 
-login.addEventListener('click', alerta);
+// login.addEventListener('click', alerta);
 
-// * function checkFill {
-//   text.innerText = 'Campos inválidos'
-//   ifinputs.value === '') {
-//     text.appendChild(formulario)
-//   }
-// }
-// buttom.addEventListener('click', checkFill)
+// // function checkFill() {
+// //   text.innerText = 'Campos inválidos'
+// //   if (inputs.value === '') {
+// //     text.appendChild(formulario)
+// //   }
+// // }
+// // buttom.addEventListener('click', checkFill)
 
 // const rightContent = document.querySelector('.right-content');
 // const error = document.createElement('h1');
-// const radio = document.querySelector('#form').gender;
+// const radio = document.querySelectorAll('#form').gender;
 // const submitForm = document.getElementById('submitForm');
 // // inputs values/////////////////
-// const name = document.getElementById('name').value;
+// const name = document.getElementById('name');
 // const sobrenome = document.getElementById('sobrenome');
 // const email = document.getElementById('celular-email');
 // const senha = document.getElementById('senha');
 // const birthdate = document.getElementById('birthdate');
 // function checkGender() {
+//   let genderSelected = ''
 //   for (let index = 0; index < 3; index += 1) {
-//     if (radio[index] === true) {
+//     if (radio[index].checked) {
+//       genderSelected = radio[index].value
 //     }
 //   }
+//   return genderSelected
 // }
-// checkGender();
+// // checkGender();
+// const genderSelect = checkGender();
 // // inputs values/////////////////
 
 // console.log(radio);
@@ -49,7 +53,7 @@ login.addEventListener('click', alerta);
 
 // function register(e) {
 //   e.preventDefault();
-//   for (let index = 0; index < inputs.length; index += 1) {
+//   for (let index = 2; index < inputs.length; index += 1) {
 //     if (inputs[index].value === '' || checkRadio() === false) {
 //       error.innerText = 'Campos inválidos';
 //       rightContent.appendChild(error);
@@ -65,10 +69,10 @@ login.addEventListener('click', alerta);
 // function submitedForm() {
 //   submitForm.classList.remove('removeElement');
 //   const p = document.getElementById('p');
-//   p.innerHTML = `Olá ${nome.value} ${sobrenome.value}`
-//   p.innerHTML = `Email: ${email.value}`
-//   p.innerHTML = `Data de nascimento: ${birthdate.value}`
-//   // p.innerHTML = `Gênero:`
+//   p.innerHTML = `Olá ${nome.value} ${sobrenome.value}<br>
+//   Email: ${email.value}<br>
+//   Data de nascimento: ${birthdate.value}<br>
+//   Gênero: ${genderSelect}`
 // }
 
 // const gender = document.getElementById('personalizado')
@@ -84,4 +88,5 @@ login.addEventListener('click', alerta);
 //     placeholder: 'Gênero (opcional)',
 //   });
 // }
+
 // gender.addEventListener('click', customGender);
